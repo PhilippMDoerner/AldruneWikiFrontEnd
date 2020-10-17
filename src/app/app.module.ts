575f98092ec08582970df1b6171d28d7596184d9
Isofruit <philippmdoerner@web.de>
2020-10-17 14:25:49 +0200

    Moved Textfield and Imagegallery component into utility

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,10 +17,10 @@ import { CreatureArticleComponent } from './components/articles/creature-article
 import { ItemArticleComponent } from './components/articles/item-article/item-article.component';
 import { RulesComponent } from './components/articles/rules/rules.component';
 import { ArticleOverviewComponent } from './components/articles/article-overview/article-overview.component';
-import { TextfieldComponent } from './components/textfield/textfield.component';
+import { TextfieldComponent } from './components/utility/textfield/textfield.component';
+import { ImageGalleryComponent } from './components/utility/image-gallery/image-gallery.component';
 
 import { CharacterService } from "src/app/services/character/character.service";
-import { ImageGalleryComponent } from './components/image-gallery/image-gallery.component';
 
 
 @NgModule({
