456c86104be996c71fccdc7be39418548cc2df5c
Isofruit <philippmdoerner@web.de>
2020-11-16 16:20:38 +0100

    Commented Transformation functions.

--- src/app/utils/functions/transform.ts
+++ src/app/utils/functions/transform.ts
@@ -1,7 +1,12 @@
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-export function transformObservableArrayContent(observable: Observable<any>, ObjectClass: any): Observable<any>{
+export function transformObservableArrayContent(observable: Observable<object[]>, ObjectClass: any): Observable<any>{
+    /**
+     * Takes an Observable of an array of object and creates new objects of type ObjectClass with it.
+     * Created for use with services, so that incoming JSON data is automatically transformed into an instance of a ModelObject.
+     * This way the Model-Data can already be associated with specific functions.
+     */
     return observable.pipe(map(contentArray => {
         const newContent: typeof ObjectClass[] = [];
         for(let item of contentArray){
@@ -12,12 +17,18 @@ export function transformObservableArrayContent(observable: Observable<any>, Obj
 }
 
 export function transformObservableContent(observable: Observable<any>, ObjectClass: any): Observable<any>{
+    /**
+     * Takes an Observable of an object and creates a new object of type ObjectClass with it.
+     * Created for use with services, so that incoming JSON data is automatically transformed into an instance of a ModelObject.
+     * This way the Model-Data can already be associated with specific functions.
+     */
     return observable.pipe(map(content => {
         return new ObjectClass(content);
     }))
 }
 
 export function TransformObservable(modelClass: any){
+    /**Decorator to apply transformObservableContent */
     return function(target: any, propertyKey: string, descriptor: PropertyDescriptor){
 
         const originalMethod = descriptor.value;
@@ -30,6 +41,7 @@ export function TransformObservable(modelClass: any){
 }
 
 export function TransformArrayObservable(modelClass: any){
+    /**Decorator to apply transformObservableArrayContent */
     return function(target: any, propertyKey: string, descriptor: PropertyDescriptor){
 
         const originalMethod = descriptor.value;
