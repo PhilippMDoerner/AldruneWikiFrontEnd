bb4fe811173231c14e52627029a9ba605aecdd87
Isofruit <philippmdoerner@web.de>
2020-12-04 16:23:24 +0100

    Added minor details to timestamp interface implementation

--- src/app/models/timestamp.ts
+++ src/app/models/timestamp.ts
@@ -2,11 +2,18 @@ export interface Timestamp{
     pk?: number,
     name: string,
     time: number,
-    encounter: string
+    encounter?: string
+    session_audio: number,
 }
 
 export class TimestampObject implements Timestamp{
+    constructor(object?: Timestamp){
+        if (object) Object.assign(this, object);
+    }
+
+    pk?: number;
     name: string;
     time: number;
-    encounter: string;
+    encounter?: string;
+    session_audio: number;
 }
