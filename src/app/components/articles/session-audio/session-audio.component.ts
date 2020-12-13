9e47cbb46b353d39978566c0a8030e0c8b13395d
Isofruit <philippmdoerner@web.de>
2020-12-13 20:33:18 +0100

    Added Session Audio Key Controls

--- src/app/components/articles/session-audio/session-audio.component.ts
+++ src/app/components/articles/session-audio/session-audio.component.ts
@@ -1,9 +1,9 @@
-import { Component, ContentChild, ContentChildren, ElementRef, OnDestroy, OnInit, ViewChild, ViewChildren, ViewContainerRef } from '@angular/core';
+import { Component, ContentChild, ContentChildren, ElementRef, HostListener, OnDestroy, OnInit, ViewChild, ViewChildren, ViewContainerRef } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subject, Subscription } from 'rxjs';
 import { first } from 'rxjs/operators';
 import { Constants } from 'src/app/app.constants';
-import { SessionAudio } from 'src/app/models/sessionaudio';
+import { SessionAudio, SessionAudioObject } from 'src/app/models/sessionaudio';
 import { Timestamp } from 'src/app/models/timestamp';
 import { SessionAudioTimestampService } from 'src/app/services/session-audio-timestamp.service';
 import { SessionAudioService } from 'src/app/services/session-audio.service';
@@ -22,15 +22,26 @@ export class SessionAudioComponent extends PermissionUtilityFunctionMixin implem
   sessionAudio: SessionAudio;
   priorSessionAudio: {isMainSessionInt: number, sessionNumber: number};
   nextSessionAudio: {isMainSessionInt: number, sessionNumber: number};
+  sessionAudioKeyControlMapping: any = {
+    "Space": this.togglePlayer,
+    "Enter": this.togglePlayer,
+    "KeyM": this.toggleSound,
+    "ArrowRight": this.seekForwards,
+    "ArrowLeft": this.seekBackwards,
+    "ArrowUp": this.increaseSound,
+    "ArrowDown": this.decreaseSound
+  }
 
   timestamps: Timestamp[];
   createTimestampEventSubject: Subject<number> = new Subject();
+  isInTimestampCreateState: boolean = false;
   
   parameter_subscription: Subscription;
+  create_timestamp_event_subscription: Subscription;
 
   @ContentChild("audioSource") audioSourceChild: ElementRef;
   @ViewChild("audioSource") audioSourceChild2: ElementRef;
-
+  @ViewChild("vimePlayer") vimePlayer: any;
 
   constructor(
     private route: ActivatedRoute,
@@ -44,7 +55,7 @@ export class SessionAudioComponent extends PermissionUtilityFunctionMixin implem
       const isMainSessionInt: number = params.isMainSession;
       const sessionNumber: number = params.sessionNumber;
 
-      this.sessionAudioService.getSessionAudioFile(isMainSessionInt, sessionNumber).pipe(first()).subscribe(sessionAudio => {
+      this.sessionAudioService.getSessionAudioFile(isMainSessionInt, sessionNumber).pipe(first()).subscribe((sessionAudio: SessionAudioObject) => {
         this.sessionAudio = sessionAudio;
         this.priorSessionAudio = sessionAudio.sessionAudioNeighbours.priorSessionAudio;
         this.nextSessionAudio = sessionAudio.sessionAudioNeighbours.nextSessionAudio;
@@ -54,10 +65,10 @@ export class SessionAudioComponent extends PermissionUtilityFunctionMixin implem
         this.timestamps = timestamps;
       })
     })
-  }
 
-  print(event){
-    console.log(event);
+    this.create_timestamp_event_subscription = this.createTimestampEventSubject.subscribe((timestampTime: number) => {
+      this.toggleTimestampCreateState();
+    })
   }
 
   routeToSessionAudio({isMainSessionInt, sessionNumber}){
@@ -74,17 +85,73 @@ export class SessionAudioComponent extends PermissionUtilityFunctionMixin implem
     }, error => console.log(error));
   }
 
-  reloadPlayer(audioSource){
-    const audioPlayer = audioSource.parentElement;
-    audioPlayer.load();
+  @HostListener('document:keydown', ["$event"]) 
+  playerControls(keyPressEvent){
+    if (this.keyAffectsPlayer(keyPressEvent)){
+      const controlFunction: Function = this.sessionAudioKeyControlMapping[keyPressEvent.code];
+      controlFunction(this.vimePlayer);
+      keyPressEvent.preventDefault();
+    }
   }
 
-  togglePlayer(vimePlayer){
+  keyAffectsPlayer(keyPressEvent): boolean{
+    const isKeyPressForPlayer: boolean = !this.isInTimestampCreateState;
+    const keyHasControlMapping: boolean = this.sessionAudioKeyControlMapping.hasOwnProperty(keyPressEvent.code);
+    return isKeyPressForPlayer && keyHasControlMapping;
+  }
+
+  toggleTimestampCreateState(): void{
+    this.isInTimestampCreateState = !this.isInTimestampCreateState;
+  }
+
+  // Player Control functions
+  togglePlayer(vimePlayer: any){
     if (vimePlayer.playing) vimePlayer.pause();
     else vimePlayer.play();
   }
 
+  toggleSound(vimePlayer: any){
+    vimePlayer.muted = !vimePlayer.muted;
+  }
+
+  seekForwards(vimePlayer: any): void{
+    let nextTime: number = vimePlayer.currentTime + 5;
+
+    if (nextTime > vimePlayer.duration){
+      nextTime = vimePlayer.duration;
+    } else if (nextTime < 0){
+      nextTime = 0;
+    }
+
+    vimePlayer.currentTime = nextTime;
+  }
+
+  seekBackwards(vimePlayer: any): void{
+    let nextTime: number = vimePlayer.currentTime - 5;
+
+    if (nextTime > vimePlayer.duration){
+      nextTime = vimePlayer.duration;
+    } else if (nextTime < 0){
+      nextTime = 0;
+    }
+
+    vimePlayer.currentTime = nextTime;
+  }
+
+  increaseSound(vimePlayer: any): void{
+    const nextVolume = vimePlayer.volume + 5;
+    if (nextVolume > 100) vimePlayer.volume = 100;
+    else vimePlayer.volume = nextVolume;
+  }
+
+  decreaseSound(vimePlayer: any): void{
+    const nextVolume = vimePlayer.volume - 5;
+    if (nextVolume < 0) vimePlayer.volume = 0;
+    else vimePlayer.volume = nextVolume;
+  }
+
   ngOnDestroy(){
     if(this.parameter_subscription) this.parameter_subscription.unsubscribe();
+    if(this.create_timestamp_event_subscription) this.create_timestamp_event_subscription.unsubscribe();
   }
 }
