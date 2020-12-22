8361cb387e964436814ba3af370f6c5b464ffca8
Isofruit <philippmdoerner@web.de>
2020-12-22 22:19:46 +0100

    Fixed File field button location

--- src/app/components/utility/file-upload/file-upload.component.ts
+++ src/app/components/utility/file-upload/file-upload.component.ts
@@ -4,7 +4,9 @@ import { FieldType } from '@ngx-formly/core';
 @Component({
   selector: 'formly-field-file',
   template: `
+  <div> 
     <input type="file" [formControl]="formControl" [formlyAttributes]="field">
+  </div>
   `,
 })
 export class FormlyFieldFile extends FieldType {}
