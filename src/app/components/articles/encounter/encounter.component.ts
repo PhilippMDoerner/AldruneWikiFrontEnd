b9d1807dcff51cadae2787c1eaab11d2bbd5674e
Isofruit <philippmdoerner@web.de>
2020-12-22 21:05:44 +0100

    Updated Encounter with warnings

--- src/app/components/articles/encounter/encounter.component.ts
+++ src/app/components/articles/encounter/encounter.component.ts
@@ -3,13 +3,15 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { first } from 'rxjs/operators';
 import { Constants } from 'src/app/app.constants';
-import { Encounter } from 'src/app/models/encounter';
+import { CharacterObject } from 'src/app/models/character';
+import { Encounter, EncounterObject } from 'src/app/models/encounter';
 import { EncounterConnectionObject, EncounterConnection } from 'src/app/models/encounterconnection';
 import { OverviewItem } from 'src/app/models/overviewItem';
 import { EncounterConnectionService } from 'src/app/services/encounter-connection.service';
 import { EncounterServiceService } from 'src/app/services/encounter/encounter-service.service';
 import { OverviewService } from 'src/app/services/overview.service';
 import { RoutingService } from 'src/app/services/routing.service';
+import { WarningsService } from 'src/app/services/warnings.service';
 
 @Component({
   selector: 'app-encounter',
@@ -19,7 +21,6 @@ import { RoutingService } from 'src/app/services/routing.service';
 export class EncounterComponent implements OnInit {
   constants: any = Constants;
   encounter: Encounter;
-  encounter_subscription: Subscription;
 
   addEncounterConnectionState: boolean = false;
   isEncounterDeleteState: boolean = false;
@@ -31,50 +32,61 @@ export class EncounterComponent implements OnInit {
     private encounterService: EncounterServiceService,
     private encounterConnectionService: EncounterConnectionService,
     private overviewService: OverviewService,
-    private router: Router,
     private route: ActivatedRoute,  
     public routingService: RoutingService,
+    private warningsService: WarningsService,
   ) { }
 
   ngOnInit(): void {
-    const pk: number = this.route.snapshot.params['pk'];
-    this.encounter_subscription = this.encounterService.getEncounter(pk).subscribe(encounter => {
-      this.encounter = encounter;
-    });
+    this.route.params.subscribe( params => {
+      const pk: number = params['pk'];
+      this.encounterService.getEncounter(pk).pipe(first()).subscribe(
+        (encounter: EncounterObject) => this.encounter = encounter,
+        error => this.routingService.routeToErrorPage(error)
+      );
+    })
   }
 
   onEncounterUpdate(updateText: string){
     this.encounter.description = updateText;
-    this.encounterService.updateEncounter(this.encounter).subscribe(encounter => {
-      this.encounter = encounter;
-    }, error => console.log(error));
+    this.encounterService.updateEncounter(this.encounter).pipe(first()).subscribe(
+      (encounter: EncounterObject) => this.encounter = encounter,
+      error => this.warningsService.showWarning(error)
+    );
   }
 
   toggleAddEncounterConnectionState(){
     this.addEncounterConnectionState = !this.addEncounterConnectionState;
     if (!this.characters){
-      this.overviewService.getOverviewItems('character').pipe(first()).subscribe(characters => {
-        this.characters = characters;
-      });
+      this.overviewService.getOverviewItems('character').pipe(first()).subscribe(
+        (characters: OverviewItem[]) => this.characters = characters,
+        error => this.warningsService.showWarning(error)
+      );
     }
   }
 
   createEncounterConnection(){
     this.baseEncounterConnection.encounter = this.encounter.pk;
-    this.encounterConnectionService.createEncounterConnection(this.baseEncounterConnection).pipe(first()).subscribe(encounterConnection => {
-      this.encounter.encounterConnections.push(encounterConnection);
-      this.resetBaseEncounterConnection();
-      this.addEncounterConnectionState = false;
-    });
+    this.encounterConnectionService.createEncounterConnection(this.baseEncounterConnection).pipe(first()).subscribe(
+      (encounterConnection: EncounterConnectionObject) => {
+        this.encounter.encounterConnections.push(encounterConnection);
+        this.resetBaseEncounterConnection();
+        this.addEncounterConnectionState = false;
+      },
+      error => this.warningsService.showWarning(error)
+    );
   }
 
-  deleteEncounterConnection(connection: EncounterConnection){
-    this.encounterConnectionService.deleteEncounterConnection(connection.pk).pipe(first()).subscribe(response => {
-      const connectionIndex: number = this.encounter.encounterConnections.indexOf(connection);
-      if (connectionIndex > -1){
-        this.encounter.encounterConnections.splice(connectionIndex, 1);
-      }
-    });
+  deleteEncounterConnection(connection: EncounterConnectionObject){
+    this.encounterConnectionService.deleteEncounterConnection(connection.pk).pipe(first()).subscribe(
+      response => {
+        const connectionIndex: number = this.encounter.encounterConnections.indexOf(connection);
+        if (connectionIndex > -1){
+          this.encounter.encounterConnections.splice(connectionIndex, 1);
+        }
+      },
+      error => this.warningsService.showWarning(error)
+    );
   }
 
   resetBaseEncounterConnection(){
@@ -88,10 +100,7 @@ export class EncounterComponent implements OnInit {
   deleteEncounter(){
     this.encounterService.deleteEncounter(this.encounter.pk).pipe(first()).subscribe(
       response => this.routingService.routeToPath('home1'),
-      error => console.log(error));
-  }
-
-  ngOnDestroy(){
-    if(this.encounter_subscription) this.encounter_subscription.unsubscribe();
+      error => this.warningsService.showWarning(error)
+    );
   }
 }
