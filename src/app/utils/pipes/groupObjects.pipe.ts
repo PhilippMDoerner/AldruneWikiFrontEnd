d43ae9582ee43849ae0ba308a8354e0085525db1
Philipp Maria Doerner <philipp3000doerner@web.de>
2021-02-11 17:19:50 +0100

    Changed sorting "groupby" pipe
    Added some documentation. Tried to make it slightly more readable. Removed unnecessary imports.
    Reversed order of elements thrown out by "groupBy".

--- src/app/utils/pipes/groupObjects.pipe.ts
+++ src/app/utils/pipes/groupObjects.pipe.ts
@@ -1,19 +1,25 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { last } from 'lodash';
 import { DiaryEntry } from 'src/app/models/diaryentry';
+import { OverviewItemObject } from 'src/app/models/overviewItem';
 import { Quest } from 'src/app/models/quest';
 
 @Pipe({name: "groupBy"}) 
 export class GroupByPipe implements PipeTransform{
     transform(itemArray: Array<any>, field: string): any{
         const callback = (accumulator: any, item: any) => {
+            //grouped Field Value = The name of the field by which you're grouping.
+            //item = The value being grouped
+            //Accumulator = Object with groupedFieldValue as key and an array of items associated with that Field as value
+            //TODO: Replace the below if-else with a ternary operator expression
             const groupedFieldValue =  this.getFieldValue(field, item);
             if (accumulator.hasOwnProperty(groupedFieldValue)) accumulator[groupedFieldValue].push(item);
             else accumulator[groupedFieldValue] = [item];
             return accumulator;
         }
-        const groupedObj = itemArray.reduce(callback, {});
-        return Object.keys(groupedObj).map(key => ({ key, value: groupedObj[key] }));
+        const groupedObj: {any : OverviewItemObject[]} = itemArray.reduce(callback, {});
+        const groupArray: {key: string, value: OverviewItemObject[]}[] = Object.keys(groupedObj).map(key => ({ key, value: groupedObj[key] }));
+
+        return groupArray.reverse() //Necessary as Diaryentries are in inverse order
     }
 
     getFieldValue(field: string, item: object): any{
