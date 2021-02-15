bc914cb8987223075765a57416c28e490a22cc81
Philipp Maria Doerner <philipp3000doerner@web.de>
2021-02-15 20:20:06 +0100

    Bugfix: Added sort to recent updates

--- src/app/components/articles/recently-updated-article/recently-updated-article.component.ts
+++ src/app/components/articles/recently-updated-article/recently-updated-article.component.ts
@@ -22,9 +22,21 @@ export class RecentlyUpdatedArticleComponent implements OnInit {
 
   ngOnInit(): void {
     this.article_subscription = this.recentlyUpdatedArticleService.getRecentlyUpdatedArticle().subscribe(
-      (articles: ArticleObject[]) => this.articles = articles,
+      (articles: ArticleObject[]) => {
+        this.articles = articles;
+        this.articles.sort(this.sortByDate);
+      },
       error => this.routingService.routeToErrorPage(error)
     );
+  }
+
+  sortByDate(article1: ArticleObject, article2: ArticleObject){
+    const date1 = new Date(article1.update_date);
+    const date2 = new Date(article2.update_date);
+    
+    if (date1 > date2) return 1;
+    if (date1 < date2) return -1;
+    return 0;
   }
 
   sidebarColor(articleType: string): string{
