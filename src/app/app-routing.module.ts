5a26848339da4d0b6a0844a3633d90629579e66a
Philipp Maria Doerner <philipp3000doerner@web.de>
2021-03-09 21:39:34 +0100

    Added route to display diaryentry with or without display mode as encounter

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -167,6 +167,12 @@ const routes: Routes = [
 		canActivate: [PermissionGuardService]
 	},
 	{
+		path: `${Constants.wikiUrlFrontendPrefixNoSlash}/diaryentry/:sessionNumber/:isMainSession/:authorName/:displayMode`,
+		component: DiaryentryArticleComponent,
+		data:{ name: "diaryentry2", requiredPermissions: [Constants.apiViewPermission]},
+		canActivate: [PermissionGuardService]
+	},
+	{
 		path: `${Constants.wikiUrlFrontendPrefixNoSlash}/diaryentry/:sessionNumber/:isMainSession/:authorName/update`,
 		component: DiaryentryArticleUpdateComponent,
 		data:{ name: "diaryentry-update", requiredPermissions: [Constants.apiUpdatePermission]},
@@ -317,7 +323,6 @@ const routes: Routes = [
 	},
 
 	// SessionAudio Routes
-	//TODO: Change this path to be :sesionNumber/:isMainSession as right now its the wrong way round
 	{
 		path: `${Constants.wikiUrlFrontendPrefixNoSlash}/sessionaudio`,
 		component: SessionAudioOverviewComponent,
