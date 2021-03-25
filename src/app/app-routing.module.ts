7f6698a84feeca96d939f21846d81fdd9d90c1a2
Philipp Maria Doerner <philipp3000doerner@web.de>
2021-03-25 20:37:26 +0100

    Fixed Diaryentries having borked toggling of updating

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -167,15 +167,15 @@ const routes: Routes = [
 		canActivate: [PermissionGuardService]
 	},
 	{
-		path: `${Constants.wikiUrlFrontendPrefixNoSlash}/diaryentry/:sessionNumber/:isMainSession/:authorName/:displayMode`,
-		component: DiaryentryArticleComponent,
-		data:{ name: "diaryentry2", requiredPermissions: [Constants.apiViewPermission]},
-		canActivate: [PermissionGuardService]
-	},
-	{
 		path: `${Constants.wikiUrlFrontendPrefixNoSlash}/diaryentry/:sessionNumber/:isMainSession/:authorName/update`,
 		component: DiaryentryArticleUpdateComponent,
 		data:{ name: "diaryentry-update", requiredPermissions: [Constants.apiUpdatePermission]},
+		canActivate: [PermissionGuardService]
+	},
+	{
+		path: `${Constants.wikiUrlFrontendPrefixNoSlash}/diaryentry/:sessionNumber/:isMainSession/:authorName/:displayMode`,
+		component: DiaryentryArticleComponent,
+		data:{ name: "diaryentry2", requiredPermissions: [Constants.apiViewPermission]},
 		canActivate: [PermissionGuardService]
 	},
 
