39e4d85531fc40d6d52b56d72490ec217f099853
Philipp Maria Doerner <philipp3000doerner@web.de>
2021-04-03 14:12:37 +0200

    Added Profile section to navbar
    Also added dropdown toggle thingy

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subject, Subscription } from 'rxjs';
 import { Constants } from 'src/app/app.constants';
 import { RoutingService } from 'src/app/services/routing.service';
 import { TokenService } from 'src/app/services/token.service';
+import { animateElement } from 'src/app/utils/functions/animationDecorator';
 
 @Component({
   selector: 'app-navbar',
@@ -12,12 +13,14 @@ import { TokenService } from 'src/app/services/token.service';
 })
 export class NavbarComponent implements OnInit, OnDestroy{
   @Input() outsideClickSubject: Subject<any>;
+
   clickSubscription: Subscription; 
 
   public searchString: string;
   public constants: any = Constants;
 
   collapsed: boolean = true; 
+  dropdownCollapsed: boolean = true;
 
   constructor(
     public tokenService: TokenService,  
@@ -25,15 +28,24 @@ export class NavbarComponent implements OnInit, OnDestroy{
   ) { }
 
   ngOnInit(): void{
-    this.clickSubscription = this.outsideClickSubject.subscribe( clickevent => this.collapsed = true);
+    this.clickSubscription = this.outsideClickSubject.subscribe( clickevent => {
+      this.collapsed = true;
+      this.dropdownCollapsed = true;
+    });
   }
 
   search(): void{
     this.routingService.routeToPath('search', {searchString: this.searchString});
   }
 
-  toggleCollapse(): void{
+  toggleCollapse(event: Event): void{
+    event.cancelBubble = true;
     this.collapsed = !this.collapsed;
+  }
+
+  toggleDropDownCollapse(event: Event): void{
+    event.cancelBubble = true;
+    this.dropdownCollapsed = !this.dropdownCollapsed;
   }
 
   logout(): void{
