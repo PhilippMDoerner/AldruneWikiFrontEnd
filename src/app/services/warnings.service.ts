f4d92a18caf4340316a207dca35d55d22bd00e54
Philipp Maria Doerner <philipp3000doerner@web.de>
2021-04-04 19:54:13 +0200

    Implemented improved warning notifications
    When a warning is shown, it is now no longer done via alert, but via an absolutely positioned card.  This card allows copying the error to the clipboard to post it to me.

--- src/app/services/warnings.service.ts
+++ src/app/services/warnings.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
-import { type } from 'os';
+import { animateElement } from '../utils/functions/animationDecorator';
+import { copyToClipboard } from '../utils/functions/copy-to-clipboard';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WarningsService {
+  notifications: any [];
 
   warnings: object = {
     0: "This can't be done without an internet connection",
@@ -18,6 +20,8 @@ export class WarningsService {
   constructor() { }
 
   showWarning(error: number | any){
+    console.log("ShowWarning error");
+    console.log(error);
     if (typeof error !== "number" && (!error.hasOwnProperty("status") || !error.hasOwnProperty("error"))) throw "Invalid error input to show warning";
     const hasSingleError: boolean = typeof error.error === "string";
     const isGenericHTTPError: boolean = typeof error === "number";
@@ -33,16 +37,15 @@ export class WarningsService {
     }
     
     const errorStatus: number = (typeof error !== "number") ? error.status : error;
-    if (typeof error !== "number" && error.hasOwnProperty("status")) error = error.status;
-    //TODO Turn this way of alerting somebody to an issue into something nicer looking
-
     const warningMessage = (this.warnings[errorStatus]) ? this.warnings[errorStatus] : this.defaultWarning;
-    alert(warningMessage + "\n\n" + httpErrorMessage);
+
+    const notificationBody = warningMessage + "\n\n" + httpErrorMessage;
+    const notificationHeading = `Error ${errorStatus}`;
+    this.showErrorNotification(notificationHeading, notificationBody, error);
   }
 
   getHttpErrorMessages(httpErrorObject: any): string{
     let httpErrorMessages: string = "The errors you received were because of:\n";
-    console.log(httpErrorObject);
 
     for(let formField in httpErrorObject.error){
       httpErrorMessages += `  ${formField}\n`;
@@ -65,6 +68,52 @@ export class WarningsService {
 
   showTextModal(text: string){
     alert(text);
+  }
+
+  showErrorNotification(heading: string, body: string, error: number | any = {}) {
+    const bodyElement: HTMLElement = document.querySelector("body");
+    const notificationHTML = `
+    <div class="card notification animate__animated animate__fadeInDown">
+        <div class="card-body">
+          <i class="fa fa-times fa-1-5x icon float-right" id="close"></i>
+          <div class="card-title">${heading}</div>
+          <hr class="white-separator">
+          <div class="card-subtitle"></div>
+          <div class="card-text">${body}</div>
+          <div class="mt-3">
+              <div class="btn btn-secondary" id="close"> <i class="fa fa-times"></i> Close </div>
+              <div class="btn btn-secondary" id="copy"> <i class="fa fa-clipboard"></i> Copy Full Error</div>
+          </div>
+        </div>
+    </div>
+    `;
+
+
+    const notificationElement: HTMLElement = document.createElement("div")
+    notificationElement.innerHTML = notificationHTML;
+    notificationElement.onclick = (event) => this.onNotificationClick(event, error);
+
+    bodyElement.appendChild(notificationElement);
+  }
+
+  onNotificationClick(event: any, error: any): void{
+    const isClickOnCloseIcon = event.target.id === "close";
+    const isClickOnCloseButton = event.target.id === "close";
+
+    if (isClickOnCloseIcon || isClickOnCloseButton){
+      const notificationElement = event.target.parentElement.parentElement;
+      animateElement(notificationElement, 'fadeOutUp')
+        .then(() => notificationElement.remove())
+    }
+
+    const isClickOnCopyErrorButton = event.target.id === "copy";
+    if(isClickOnCopyErrorButton) this.copyErrorToClipboard(error)
+  }
+
+  copyErrorToClipboard(error: number | any){
+    const isHTTPErrorStatus = typeof error === "number";
+    const errorString = (isHTTPErrorStatus) ? `Error status ${error}` : JSON.stringify(error, null, "    ");
+    copyToClipboard(errorString);
   }
 
 }
