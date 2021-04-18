75e9a2aa9decfa43c23c1a588e303446a65c8356
Philipp Maria Doerner <philipp3000doerner@web.de>
2021-04-18 15:35:11 +0200

    Removed unused imports

--- src/app/services/overview.service.ts
+++ src/app/services/overview.service.ts
@@ -1,9 +1,8 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Constants } from '../app.constants';
 import { OverviewItem, OverviewItemObject } from "src/app/models/overviewItem";
 import { Observable } from 'rxjs';
-import { UserService } from './user.service';
 import { TransformArrayObservable } from '../utils/functions/transform';
 
 @Injectable({
