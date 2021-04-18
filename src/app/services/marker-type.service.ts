2bda4ba0eecd992d2f439bc7bceb65c671533334
Philipp Maria Doerner <philipp3000doerner@web.de>
2021-04-18 15:31:21 +0200

    Removed unused imports

--- src/app/services/marker-type.service.ts
+++ src/app/services/marker-type.service.ts
@@ -1,9 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { Constants } from '../app.constants';
-import { MapMarker } from '../models/mapmarker';
-import { MapMarkerType } from '../models/mapmarkertype';
 import { GenericService } from './generic.service';
 
 @Injectable({
