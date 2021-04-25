4979b036104db19a84380ea22bb27d2079ae535e
Philipp Maria Doerner <philipp3000doerner@web.de>
2021-04-25 13:15:22 +0200

    Implemented ArticleFormMixin for marker

--- src/app/components/articles/marker-update/marker-update.component.ts
+++ src/app/components/articles/marker-update/marker-update.component.ts
@@ -1,32 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormlyFieldConfig } from '@ngx-formly/core';
-import { Observable, Subscription } from 'rxjs';
+import { Subscription } from 'rxjs';
 import { first } from 'rxjs/operators';
-import { Constants } from 'src/app/app.constants';
-import { Location, LocationObject } from 'src/app/models/location';
+import { LocationObject } from 'src/app/models/location';
 import { MapMarker, MapMarkerObject } from 'src/app/models/mapmarker';
 import { LocationService } from 'src/app/services/location/location.service';
 import { MarkerService } from 'src/app/services/marker.service';
 import { MyFormlyService } from 'src/app/services/my-formly.service';
 import { RoutingService } from 'src/app/services/routing.service';
 import { WarningsService } from 'src/app/services/warnings.service';
+import { ArticleFormMixin } from 'src/app/utils/functions/articleFormMixin';
 
 @Component({
   selector: 'app-marker-update',
   templateUrl: './marker-update.component.html',
   styleUrls: ['./marker-update.component.scss']
 })
-export class MarkerUpdateComponent implements OnInit {
-  private parameter_subscription: Subscription;
+export class MarkerUpdateComponent extends ArticleFormMixin implements OnInit {
+  //Defining ArticleFormMixin Properties
+  userModel: MapMarkerObject;
+  serverModel: MapMarker;
+  updateCancelRoute = {routeName: 'marker', params: {location_name: null, parent_location_name: null, map_name: null}};
+  creationCancelRoute = {routeName: 'location', params: {name: null, parent_name: null}};
 
-  constants: any = Constants;
-
-  formState: string;
-
-  model: MapMarker;
-  fields: FormlyFieldConfig[] = [
+  formlyFields: FormlyFieldConfig[] = [
     this.formlyService.genericInput({key: "latitude", isNumberInput: true}),
     this.formlyService.genericInput({key: "longitude", isNumberInput: true}),
     this.formlyService.genericSelect({key: "location", optionsType: 'location'}),
@@ -36,36 +34,52 @@ export class MarkerUpdateComponent implements OnInit {
     this.formlyService.genericInput({key: "icon", label: "Custom Icon", required: false}),
   ];
 
+  //Custom properties
+  private parameter_subscription: Subscription;
+
   constructor(
-    private markerService: MarkerService,
+    markerService: MarkerService,
     private locationService: LocationService,
-    private router: Router,
+    router: Router,
     private route: ActivatedRoute,
     private formlyService: MyFormlyService,
-    private warnings: WarningsService,  
+    public warnings: WarningsService,  
     public routingService: RoutingService,
-  ) { }
+  ) { 
+    super(
+      router,
+      routingService,
+      warnings,
+      markerService
+    )
+  }
 
   ngOnInit(): void {
-    this.formState = (this.router.url.includes("update")) ? Constants.updateState : Constants.createState;
-
     this.parameter_subscription = this.route.params.subscribe(params =>{
       const parentLocationName = params['parent_location_name'];
       const locationName = params['location_name'];
+      const mapName: string = params['map_name'];
+
+      //Update Cancel Route Params
+      this.updateCancelRoute.params.location_name = locationName;
+      this.updateCancelRoute.params.parent_location_name = parentLocationName;
+      this.updateCancelRoute.params.map_name = mapName; //Undefined if route of this component is 'marker-create'
+      this.creationCancelRoute.params.name = locationName;
+      this.creationCancelRoute.params.parent_name = parentLocationName;
       
-      if (this.formState === Constants.updateState){
-        const mapName: string = params['map_name'];
-        this.markerService.getMapMarker(parentLocationName, locationName, mapName).pipe(first()).subscribe(
-          (marker: MapMarkerObject) => this.model = marker,
+      //Get Marker
+      if (this.isInUpdateState()){
+        this.articleService.readByParam({parentLocationName, locationName, mapName}).pipe(first()).subscribe(
+          (marker: MapMarkerObject) => this.userModel = marker,
           error => this.routingService.routeToErrorPage(error)
         );
 
-      } else if (this.formState === Constants.createState){
+      } else if (this.isInCreateState()){
         this.locationService.readByParam({parentLocationName, locationName}).pipe(first()).subscribe(
           (location: LocationObject) => {
-            this.model = new MapMarkerObject();
-            this.model.location = location.pk;
-            this.model.location_details = {
+            this.userModel = new MapMarkerObject();
+            this.userModel.location = location.pk;
+            this.userModel.location_details = {
               parent_location_name: location.parent_location_details.name,
               name: location.name,
               description: location.description,
@@ -77,42 +91,6 @@ export class MarkerUpdateComponent implements OnInit {
       }
 
     })
-  }
-
-  onSubmit(){
-    const isFormInUpdateState: boolean = (this.formState === Constants.updateState);
-    const responseObservable: Observable<MapMarkerObject> =  isFormInUpdateState ? 
-        this.markerService.update(this.model.pk, this.model) : 
-        this.markerService.create(this.model);
-    
-    responseObservable.pipe(first()).subscribe(
-      (marker: MapMarkerObject) => this.routingService.routeToPath('marker', {
-          parent_location_name: marker.location_details.parent_location_name,
-          location_name: marker.location_details.name,
-          map_name: marker.map_details.name
-        }
-      ),
-      error => this.warnings.showWarning(error)
-    );
-  }
-
-  onCancel(){
-    const isFormInUpdateState : boolean = (this.formState === Constants.updateState)
-    const params = this.route.snapshot.params;
-
-    if (isFormInUpdateState){
-      this.routingService.routeToPath('marker', {
-        location_name: params.location_name,
-        parent_location_name: params.parent_location_name,
-        map_name: params.map_name,
-      });
-
-    } else {
-      this.routingService.routeToPath('location', {
-        name: params.location_name,
-        parent_name: params.parent_location_name
-      });
-    } 
   }
 
   ngOnDestroy(){
