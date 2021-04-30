5e9421794c2cb79b62ad0118abaa67f772ef0fc5
Philipp Maria Doerner <philipp3000doerner@web.de>
2021-04-30 19:05:26 +0200

    Implemented ArticleMixin to characters

--- src/app/components/articles/character-article/character-article.component.ts
+++ src/app/components/articles/character-article/character-article.component.ts
@@ -10,6 +10,7 @@ import { WarningsService } from 'src/app/services/warnings.service';
 import { RoutingService } from 'src/app/services/routing.service';
 import { QuoteObject } from 'src/app/models/quote';
 import { CharacterPlayerClassConnection } from 'src/app/models/playerclass';
+import { ArticleMixin } from 'src/app/utils/functions/articleMixin';
 
 @Component({
   selector: 'app-character-article',
@@ -17,61 +18,34 @@ import { CharacterPlayerClassConnection } from 'src/app/models/playerclass';
   styleUrls: ['./character-article.component.scss']
 })
 
-export class CharacterArticleComponent extends PermissionUtilityFunctionMixin implements OnInit, OnDestroy {
-  constants: any = Constants;
-  character: CharacterObject;
-  confirmationModal: Subject<void> = new Subject<void>();
-  articleType: string = 'character';
+export class CharacterArticleComponent extends ArticleMixin implements OnInit, OnDestroy {
+  //ArticleMixin Variables
+  articleData: CharacterObject;
+  deleteRoute = {routeName: "character-overview", params: {}}
+  queryParameterName = "name";
 
+  //Custom Variables
   quote: QuoteObject;
   quoteCreateState: boolean = false;
 
-  private parameter_subscription: Subscription;
-
   constructor(
-    private characterService: CharacterService,
-    private route: ActivatedRoute,
-    private warnings: WarningsService,  
+    characterService: CharacterService,
+    public route: ActivatedRoute,
+    public warnings: WarningsService,  
     public routingService: RoutingService,
-    ) { super() }
-
-  ngOnInit(): void {
-    this.parameter_subscription = this.route.params.subscribe( params => {
-      const characterName: string = params.name;
-      this.characterService.readByParam(characterName).pipe(first()).subscribe(
-        (character: CharacterObject) => this.character = character, 
-        error => this.routingService.routeToErrorPage(error)
-      );
-
-    });
-  }
-
-  onDescriptionUpdate(updatedDescription){
-    const oldDescription = this.character.description;
-    this.character.description = updatedDescription;
-    this.characterService.update(this.character.pk, this.character).pipe(first()).subscribe(
-      (character: CharacterObject) => {},
-      error =>{
-        this.character.description = oldDescription;
-        this.warnings.showWarning(error);
-      }
-    );
-  }
+    ) { 
+      super(
+        characterService,
+        route,
+        routingService,
+        warnings
+      ) 
+    }
 
   createPlayerClassString(){
-    return this.character.player_class_connections
+    return this.articleData.player_class_connections
       .map((connection: CharacterPlayerClassConnection) => connection.player_class_details.name)
       .join(", ");
   }
 
-  deleteArticle(){
-      this.characterService.delete(this.character.pk).pipe(first()).subscribe(
-        response => this.routingService.routeToPath('character-overview'),
-        error => this.warnings.showWarning(error)
-      )
-  }
-
-  ngOnDestroy(){
-    if (this.parameter_subscription) this.parameter_subscription.unsubscribe();
-  }
 }
