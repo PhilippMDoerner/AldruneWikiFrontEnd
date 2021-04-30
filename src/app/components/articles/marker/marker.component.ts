bc70da6bf183deb6d529cc829a15a6bfcebdd5b3
Philipp Maria Doerner <philipp3000doerner@web.de>
2021-04-30 19:51:09 +0200

    Implemented ArticleMixin for marker

--- src/app/components/articles/marker/marker.component.ts
+++ src/app/components/articles/marker/marker.component.ts
@@ -7,54 +7,50 @@ import { MapMarker, MapMarkerObject } from 'src/app/models/mapmarker';
 import { MarkerService } from 'src/app/services/marker.service';
 import { RoutingService } from 'src/app/services/routing.service';
 import { WarningsService } from 'src/app/services/warnings.service';
+import { ArticleMixin } from 'src/app/utils/functions/articleMixin';
 
 @Component({
   selector: 'app-marker',
   templateUrl: './marker.component.html',
   styleUrls: ['./marker.component.scss']
 })
-export class MarkerComponent implements OnInit {
-  constants: any = Constants;
-  marker: MapMarkerObject;
-
-  parameter_subscription: Subscription;
+export class MarkerComponent extends ArticleMixin implements OnInit {
+  //ArticleMixin Variables
+  articleData: MapMarkerObject;
+  deleteRoute = {routeName: "location", params: {name: null, parent_name: null}}
 
   constructor(
-    private route: ActivatedRoute,
-    private markerService: MarkerService,
-    private warnings: WarningsService,  
+    public route: ActivatedRoute,
+    markerService: MarkerService,
+    public warnings: WarningsService,  
     public routingService: RoutingService,
-  ) { }
+  ) {
+    super(
+      markerService,
+      route,
+      routingService,
+      warnings
+    )
+   }
 
   ngOnInit(): void {
     this.parameter_subscription = this.route.params.subscribe(params =>{
       const parentLocationName: string = params['parent_location_name'];
       const locationName: string = params['location_name'];
       const mapName: string = params['map_name'];
-      this.markerService.readByParam({parentLocationName, locationName, mapName}).pipe(first()).subscribe(
-        (marker: MapMarkerObject) => this.marker = marker,
+
+      //Set params for delete route
+      //Set Parameters for delete route
+      this.deleteRoute.params.name = locationName;
+      this.deleteRoute.params.parent_name = parentLocationName;
+
+      //Get marker
+      this.articleService.readByParam({parentLocationName, locationName, mapName}).pipe(first()).subscribe(
+        (marker: MapMarkerObject) => this.articleData = marker,
         error => this.routingService.routeToErrorPage(error)
       ); 
     })
 
-  }
-
-  deleteMarker(){
-    const parentLocationName: string = this.marker.location_details.parent_location_name;
-    const locationName: string = this.marker.location_details.name;
-
-    this.markerService.delete(this.marker.pk).pipe(first()).subscribe(
-      response =>  this.routingService.routeToPath('location', {
-          name: locationName, 
-          parent_name: parentLocationName
-        }
-      ),
-      error => this.warnings.showWarning(error)
-    )
-  }
-
-  ngOnDestroy(){
-    if (this.parameter_subscription) this.parameter_subscription.unsubscribe();
   }
 
 }
