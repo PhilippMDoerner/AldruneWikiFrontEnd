49fe3678b2223fae28aa49edee05e2a5cb41fd3c
Philipp Maria Doerner <philipp3000doerner@web.de>
2021-04-30 19:55:57 +0200

    Implemented ArticleMixin in organization

--- src/app/components/articles/organization-article/organization-article.component.ts
+++ src/app/components/articles/organization-article/organization-article.component.ts
@@ -7,59 +7,31 @@ import { Organization, OrganizationObject } from 'src/app/models/organization';
 import { OrganizationService } from 'src/app/services/organization/organization.service';
 import { RoutingService } from 'src/app/services/routing.service';
 import { WarningsService } from 'src/app/services/warnings.service';
+import { ArticleMixin } from 'src/app/utils/functions/articleMixin';
 
 @Component({
   selector: 'app-organization-article',
   templateUrl: './organization-article.component.html',
   styleUrls: ['./organization-article.component.scss']
 })
-export class OrganizationArticleComponent implements OnInit {
-  constants: any = Constants;
-  organization: Organization;
-  articleType: string = 'organization';
+export class OrganizationArticleComponent extends ArticleMixin implements OnInit {
+  //ArticleMixinVariables
+  articleData: Organization;
+  deleteRoute = {routeName: "organization-overview", params: {}};
+  queryParameterName: string = 'name';
 
-  private parameter_subscription: Subscription;
 
   constructor(
-    private organizationService: OrganizationService,
-    private route: ActivatedRoute,
-    private warnings: WarningsService,  
+    organizationService: OrganizationService,
+    public route: ActivatedRoute,
+    public warnings: WarningsService,  
     public routingService: RoutingService,
-  ) { }
-
-  ngOnInit(): void {
-    this.parameter_subscription = this.route.params.subscribe(params => {
-      const organizationName = params.name;
-
-      this.organizationService.readByParam(organizationName).pipe(first()).subscribe(
-        (organization: OrganizationObject) => this.organization = organization,
-        error => this.routingService.routeToErrorPage(error)
-      );
-    })
-
-  }
-
-  onDescriptionUpdate(updatedDescription){
-    const oldDescription = this.organization.description;
-    this.organization.description = updatedDescription;
-
-    this.organizationService.update(this.organization.pk, this.organization).pipe(first()).subscribe(
-      (organization: OrganizationObject) => {},
-      error =>{
-        this.organization.description = oldDescription;
-        this.warnings.showWarning(error);
-      }
-    );
-  }
-
-  deleteArticle(){
-    this.organizationService.delete(this.organization.pk).pipe(first()).subscribe(
-      response => this.routingService.routeToPath('organization-overview'),
-      error => this.warnings.showWarning(error)
-    );
-  }
-
-  ngOnDestroy(){
-    if (this.parameter_subscription) this.parameter_subscription.unsubscribe();
-  }
+  ) {
+    super(
+      organizationService,
+      route,
+      routingService,
+      warnings
+    )
+   }
 }
