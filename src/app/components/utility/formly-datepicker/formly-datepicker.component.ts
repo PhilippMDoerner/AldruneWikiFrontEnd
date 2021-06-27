61b239bed7b6bbdcd08eb27274515d577c808cd8
Philipp Maria Doerner <philipp3000doerner@web.de>
2021-06-27 20:43:14 +0200

    Removed unnecessary code

--- src/app/components/utility/formly-datepicker/formly-datepicker.component.ts
+++ src/app/components/utility/formly-datepicker/formly-datepicker.component.ts
@@ -64,17 +64,11 @@ import { FieldType } from '@ngx-formly/core';
   ]
 })
 export class FormlyDatepickerComponent extends FieldType {
-  // TODO: Fix the bug about the datepicker being updated during initialization of the view.
-  // TODO: Fix the initial date not being displayed in the datepicker input field
+
   constructor(
     private ngbCalendar: NgbCalendar,
     private dateAdapter: NgbDateAdapter<string>
   ) { super() }
-
-  inputDateToField(event: NgbDate){
-    console.log(this.model);
-    //this.model.session_date = `${event.year}-${event.month}-${event.day}`;
-  }
 
 
   get today() {
