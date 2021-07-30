959f2005525252524ec53f4d6db03337de51c876
Philipp Maria Doerner <philipp3000doerner@web.de>
2021-07-30 22:57:18 +0200

    Updated sidebar to take entry data from constants instead

--- src/app/components/utility/sidebar/sidebar.component.ts
+++ src/app/components/utility/sidebar/sidebar.component.ts
@@ -13,69 +13,7 @@ export class SidebarComponent implements OnInit {
 
   @Input() showSidebar: BehaviorSubject<boolean>;
 
-  sidebarEntries: any = [
-    {
-      title: "Home",
-      iconClass: "fa fa-home",
-      route: this.routingService.getRoutePath("home2")
-    },
-    {
-      title: "Creatures", 
-      iconClass: "fas fa-dragon", 
-      route: this.routingService.getRoutePath("creature-overview")
-    },
-    {
-      title: "Characters", 
-      iconClass: "fas fa-male", 
-      route: this.routingService.getRoutePath("character-overview")
-    },
-    {
-      title: "DiaryEntries", 
-      iconClass: "fas fa-book-open", 
-      route: this.routingService.getRoutePath("diaryentry-overview")
-    },
-    {
-      title: "Items", 
-      iconClass: "fa fa-magic", 
-      route: this.routingService.getRoutePath("item-overview")
-    },
-    {
-      title: "Locations", 
-      iconClass: "fas fa-compass", 
-      route: this.routingService.getRoutePath("location-overview")
-    },
-    {
-      title: "Organizations", 
-      iconClass: "fas fa-sitemap", 
-      route: this.routingService.getRoutePath("organization-overview")
-    },
-    {
-      title: "Maps", 
-      iconClass: "fa fa-map", 
-      route: this.routingService.getRoutePath('map', {name: Constants.defaultMapName})
-    },
-    {
-      title: "Quests", 
-      iconClass: "fas fa-question-circle", 
-      route: this.routingService.getRoutePath("quest-overview")
-    },
-    {
-      title: "Spells", 
-      iconClass: "fas fa-hand-sparkles", 
-      route: this.routingService.getRoutePath("spells")
-    },
-    {
-      title: "Rules", 
-      iconClass: "fa fa-book", 
-      route: this.routingService.getRoutePath("rules")
-    },
-    {
-      title: "Recordings", 
-      iconClass: "fa fa-file-audio-o", 
-      route: this.routingService.getRoutePath("sessionaudio-overview")
-    },
-
-  ]
+  sidebarEntries: any;
 
   //TODO: customizeable sidebar order of menu items
   constructor(
@@ -83,5 +21,15 @@ export class SidebarComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    const allMetaData: any = this.constants.articleTypeMetaData;
+    const sidebarEntries = allMetaData.filter(metaDataEntry => metaDataEntry.showInSidebar)
+    const processedEntries = sidebarEntries.map(metaDataEntry => {
+        const routeName: string = metaDataEntry.route
+        const routeUrl: string = this.routingService.getRoutePath(routeName);
+        metaDataEntry.route = routeUrl;
+        return metaDataEntry;
+    });
+    
+    this.sidebarEntries = processedEntries;
   }
 }
