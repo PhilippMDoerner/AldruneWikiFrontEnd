2c804f4268f5399f8a403f64ba7af99704d0e930
Philipp Maria Doerner <philipp3000doerner@web.de>
2021-08-08 11:16:36 +0200

    Fixed sidebar admin section not properly updating when admin user logs out.

--- src/app/components/utility/sidebar/sidebar.component.ts
+++ src/app/components/utility/sidebar/sidebar.component.ts
@@ -1,15 +1,17 @@
-import { Component, ElementRef, Input, OnChanges, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, OnChanges, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Constants } from 'src/app/app.constants';
 import { RoutingService } from 'src/app/services/routing.service';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Subscription } from 'rxjs';
 import { TokenService } from 'src/app/services/token.service';
+import { ActivatedRoute, Router, RouterEvent } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, OnDestroy {
   constants = Constants;
 
   @Input() showSidebar: BehaviorSubject<boolean>;
@@ -18,10 +20,13 @@ export class SidebarComponent implements OnInit {
   showUserSection: boolean = false;
   showAdminSection: boolean = false;
 
+  routingSubscription: Subscription;
+
   //TODO: customizeable sidebar order of menu items
   constructor(
     public routingService: RoutingService,
-    public tokenService: TokenService
+    public tokenService: TokenService,
+    private router: Router,
   ) { }
 
   ngOnInit(): void {
@@ -35,7 +40,19 @@ export class SidebarComponent implements OnInit {
     });
     
     this.sidebarEntries = processedEntries;
-    this.showAdminSection = this.tokenService.isAdmin() || this.tokenService.isSuperUser();
+
+    //Updates whether admin section should be shown in the sidebar or not with every update of the route
+    this.routingSubscription = this.router.events.pipe(
+      //Ensures you fire only one routingevent per routing
+      filter((e: any) => {
+        const isRouterEvent: boolean =  e instanceof RouterEvent;
+        const isSingleRouterEvent: boolean = e.navigationTrigger === "imperative";
+        return isRouterEvent && isSingleRouterEvent
+      })
+    ).subscribe(
+      _ => {
+        this.showAdminSection = this.tokenService.isAdmin() || this.tokenService.isSuperUser();
+    });
   }
 
   logout(): void{
@@ -45,5 +62,9 @@ export class SidebarComponent implements OnInit {
     }
 
     this.routingService.routeToPath('login-state', {state: 'logged-out'});
+  }
+
+  ngOnDestroy(): void{
+    if (this.routingSubscription) this.routingSubscription.unsubscribe();
   }
 }
