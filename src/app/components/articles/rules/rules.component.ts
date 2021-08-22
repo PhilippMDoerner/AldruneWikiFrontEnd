6eb1faaa6f6dfb029ba20065c729d0d4622a38bf
Philipp Maria Doerner <philipp3000doerner@web.de>
2021-08-22 09:25:28 +0200

    Made rules campaign compliant

--- src/app/components/articles/rules/rules.component.ts
+++ src/app/components/articles/rules/rules.component.ts
@@ -7,6 +7,7 @@ import { Rule, RuleObject } from "src/app/models/rule";
 import { RoutingService } from 'src/app/services/routing.service';
 import { RuleService } from 'src/app/services/rule.service';
 import { TokenService } from 'src/app/services/token.service';
+import { WarningsService } from 'src/app/services/warnings.service';
 @Component({
   selector: 'app-rules',
   templateUrl: './rules.component.html',
@@ -16,6 +17,8 @@ export class RulesComponent implements OnInit, AfterViewInit {
   rules: Rule[];
   panelIsOpenArray: boolean[];
   constants: any = Constants;
+  parameterSubscription: Subscription;
+  campaign: string;
 
   @ViewChildren("ruleElements") ruleComponents: QueryList<any>;
 
@@ -23,20 +26,27 @@ export class RulesComponent implements OnInit, AfterViewInit {
     private ruleService: RuleService,
     public routingService: RoutingService,
     public tokenService: TokenService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private warning: WarningsService
   ) { }
 
   ngOnInit(): void {
-    this.ruleService.list().pipe(first()).subscribe( 
-      (rules: RuleObject[]) => {
-        this.rules = rules.sort((rule1, rule2) => rule1.name < rule2.name ? -1 : 1);
+    this.parameterSubscription = this.route.params.subscribe(
+      params => {
+        this.campaign = params.campaign;
 
-        this.panelIsOpenArray = [];
-        for (let rule of rules){
-          this.panelIsOpenArray.push(true);
-        }
-      },
-      error => this.routingService.routeToErrorPage(error)
+        this.ruleService.campaignList(this.campaign).pipe(first()).subscribe( 
+          (rules: RuleObject[]) => {
+            this.rules = rules.sort((rule1, rule2) => rule1.name < rule2.name ? -1 : 1);
+    
+            this.panelIsOpenArray = [];
+            for (let rule of rules){
+              this.panelIsOpenArray.push(true);
+            }
+          },
+          error => this.routingService.routeToErrorPage(error)
+        );
+      }
     )
   }
 
