3ecf4ac9ab282f8d4acc4b810e2158623d2bd6c4
Philipp Maria Doerner <philipp3000doerner@web.de>
2021-08-29 20:46:33 +0200

    Added API endpoints to swap encounter order indices, insert encounters into a diaryentry and cut-insert encounters

--- src/app/services/encounter/encounter-service.service.ts
+++ src/app/services/encounter/encounter-service.service.ts
@@ -3,7 +3,7 @@ import { Encounter, EncounterObject } from "src/app/models/encounter";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Constants } from "src/app/app.constants";
 import { Observable } from "rxjs";
-import { TransformArrayObservable, TransformObservable } from 'src/app/utils/functions/transform';
+import { TransformArrayObservable, TransformObservable, transformObservableArrayContent, transformObservableContent } from 'src/app/utils/functions/transform';
 import { GenericObjectService } from '../generic-object.service';
 
 const httpOptions = {
@@ -18,10 +18,36 @@ export class EncounterServiceService extends GenericObjectService {
 
   constructor(http: HttpClient) { super(http, EncounterObject) }
 
-  @TransformArrayObservable(EncounterObject)
-  getDiaryEntryEncounters(session_pk: number, authorName: string): Observable<Encounter[]>{
+  getDiaryEntryEncounters(session_pk: number, authorName: string): Observable<EncounterObject[]>{
     const url = `${this.baseUrl}/session/${session_pk}/${authorName}`;
-    return this.http.get<Encounter[]>(url);
+    const encounterObs: Observable<Encounter[]> = this.http.get<Encounter[]>(url);
+    return transformObservableArrayContent(encounterObs, EncounterObject);
+  }
+
+  createForDiaryentry(campaign: string, encounter: Encounter): Observable<EncounterObject[]>{
+    const url = `${this.baseUrl}/${campaign}/diaryentryinsert/`;
+    const encounterObs: Observable<Encounter[]> = this.http.post<Encounter[]>(url, encounter);
+    return transformObservableArrayContent(encounterObs, EncounterObject);
+  }
+
+  swapEncounterOrder(campaign: string, encounter1_pk: number, encounter2_pk: number): Observable<EncounterObject[]>{
+    const url = `${this.baseUrl}/${campaign}/orderswap/`;
+    const requestBody = {"encounter1": encounter1_pk, "encounter2": encounter2_pk};
+
+    const swapObs: Observable<Encounter[]> = this.http.patch<Encounter[]>(url, requestBody);
+    return transformObservableArrayContent(swapObs, EncounterObject);
+  }
+
+  cutInsertEncounter(campaign: string, encounter: Encounter, newOrderIndex: number): Observable<EncounterObject[]>{
+    const url = `${this.baseUrl}/${campaign}/cutinsert/`;
+    const requestBody = {
+      "encounter": encounter.pk, 
+      "old_order_index": encounter.order_index, 
+      "new_order_index": newOrderIndex
+    };
+    
+    const cutInsertObs: Observable<Encounter[]> = this.http.patch<Encounter[]>(url, requestBody);
+    return transformObservableArrayContent(cutInsertObs, EncounterObject);
   }
 
 }
