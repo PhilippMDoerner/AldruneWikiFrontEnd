d7cde22d5e9502530573d8ad7a703e30552a450b
Philipp Maria Doerner <philipp3000doerner@web.de>
2021-09-05 21:28:52 +0200

    Updated campaign models to work with guest groups

--- src/app/models/campaign.ts
+++ src/app/models/campaign.ts
@@ -3,32 +3,41 @@ import { Image } from './image';
 import { Constants } from '../app.constants';
 import { User } from './user';
 
-export interface Campaign extends CampaignOverview{
+export interface Campaign{
     name: string;
     pk?: number;
+    background_image?: string;
+
     members?: User[];
     admins?: User[];
+    guests?: User[];
+
     member_group_name?: string;
     admin_group_name?: string;
+    guest_group_name?: string;
 }
 
 export interface CampaignOverview extends ArticleObject{
     name: string;
     pk?: number;
     background_image?: string;
+
     isMember: boolean;
     isAdmin: boolean;
+    isGuest: boolean;
 }
 
 export class CampaignObject implements Campaign{
     pk?: number;
     name: string;
+
     members?: User[];
     admins?: User[];
+    guests?: User[];
+    
     member_group_name?: string;
     admin_group_name?: string;
-    isMember: boolean;
-    isAdmin: boolean;
+    guest_group_name?: string;
 
     constructor(object?: Campaign){
         if (object) Object.assign(this, object);
