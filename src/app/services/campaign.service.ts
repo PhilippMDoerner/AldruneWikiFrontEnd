ac42fe852e35a9d7340f1e243f5e9a2bffe0d04b
Philipp Maria Doerner <philipp3000doerner@web.de>
2021-09-05 10:35:17 +0200

    Added the ability to cache campaign data and only change it upon swapping campaigns

--- src/app/services/campaign.service.ts
+++ src/app/services/campaign.service.ts
@@ -1,20 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { first } from 'rxjs/operators';
 import { Constants } from '../app.constants';
-import { CampaignOverview } from '../models/campaign';
+import { Campaign, CampaignOverview } from '../models/campaign';
 import { User } from '../models/user';
 import { GenericService } from './generic.service';
+import { WarningsService } from './warnings.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CampaignService extends GenericService{
   baseUrl: string = `${Constants.wikiApiUrl}/campaign`;
+  currentCampaignData: BehaviorSubject<Campaign> = new BehaviorSubject(null);
 
-  constructor(http : HttpClient) { 
+  constructor(
+    http : HttpClient,
+    private warning: WarningsService
+  ) { 
     super(http)  //Second param indicates which class the data of this service is turned into
   }
+
+
 
   campaignList(): Observable<any[]>{
     return this.http.get<CampaignOverview[]>(`${this.baseUrl}/overview/`);
@@ -50,6 +58,24 @@ export class CampaignService extends GenericService{
   removeAdmin(campaign: string, user: User): Observable<User[]>{
     const requestBody = {action: "remove_admin", user}
     return this.http.patch<User[]>(`${this.baseUrl}/${campaign}/members/`, requestBody);
+  }
+
+  getCampaignData(): BehaviorSubject<Campaign>{
+    return this.currentCampaignData;
+  }
+
+  updateCampaignData(campaignName: string): void{
+    console.log("Asking service to update the subject with " + campaignName);
+    if(campaignName == null) return //Contemplate putting in some default campaign data
+
+    const currentCampaign: Campaign = this.currentCampaignData.value;
+    const isCurrentCampaign: boolean = currentCampaign.name.toLowerCase() === campaignName.toLowerCase();
+    if (isCurrentCampaign) return;
+    
+    this.readByParam(campaignName).pipe(first()).subscribe(
+      (newCampaignData: Campaign) => this.currentCampaignData.next(newCampaignData),
+      error => this.warning.showWarning(error)
+    );
   }
 
 }
