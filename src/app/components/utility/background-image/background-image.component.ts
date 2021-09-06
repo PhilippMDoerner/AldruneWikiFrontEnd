1736f6f99da8baa12e6feb3bf6dc0adb693d700e
Philipp Maria Doerner <philipp3000doerner@web.de>
2021-09-06 20:04:09 +0200

    Made it so updates to the campaign set trigger a re-checking of the background images

--- src/app/components/utility/background-image/background-image.component.ts
+++ src/app/components/utility/background-image/background-image.component.ts
@@ -1,13 +1,9 @@
 import { Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
-import { filter, first } from 'rxjs/operators';
+import { filter } from 'rxjs/operators';
 import { Constants } from 'src/app/app.constants';
-import { Campaign, CampaignOverview } from 'src/app/models/campaign';
-import { CampaignService } from 'src/app/services/campaign.service';
+import { CampaignOverview } from 'src/app/models/campaign';
 import { GlobalUrlParamsService } from 'src/app/services/global-url-params.service';
-import { WarningsService } from 'src/app/services/warnings.service';
-import { animateElement } from 'src/app/utils/functions/animationDecorator';
 
 @Component({
   selector: 'app-background-image',
@@ -20,37 +16,37 @@ export class BackgroundImageComponent implements OnInit, OnDestroy {
   currentCampaign: CampaignOverview;
 
   urlParamSubscription: Subscription;
+  campaignSetSubscription: Subscription;
 
   @ViewChild('backgroundImage') backgroundImage: ElementRef;
 
   constructor(
-    private campaignService: CampaignService,
-    private warningService: WarningsService,
     private globalURLParamService: GlobalUrlParamsService
   ) { }
 
   ngOnInit(): void {
-    this.campaignService.campaignList().pipe(first()).subscribe(
-      (campaignOverviewData: CampaignOverview[]) => {
-        this.allCampaignData = campaignOverviewData;
-        
-        this.urlParamSubscription = this.globalURLParamService.getURLCampaignParameter()
-          .pipe(filter((campaignName: string) => this.currentCampaign?.name !== campaignName))
-          .subscribe((campaignName: string) => this.updateCurrentCampaign(campaignName));
-      },
-      error => this.warningService.showWarning(error)
-    );
+    this.campaignSetSubscription = this.globalURLParamService.getCampaigns()
+      .subscribe((campaigns: CampaignOverview[]) =>  this.updateCurrentCampaignSet(campaigns));
 
-
+    this.urlParamSubscription = this.globalURLParamService.getURLCampaignParameter()
+      .pipe(filter((campaignName: string) => this.currentCampaign?.name !== campaignName))
+      .subscribe((campaignName: string) => this.updateCurrentCampaign(campaignName));
   }
 
   async updateCurrentCampaign(campaignName: string): Promise<void>{
-    if (this.backgroundImage == null) return;
-
-    await animateElement(this.backgroundImage.nativeElement, 'fadeOut');
+    if (this.backgroundImage == null || this.allCampaignData == null) return;
     this.currentCampaign = this.findCampaignByName(campaignName);
-    await animateElement(this.backgroundImage.nativeElement, 'fadeIn');
   }
+
+  async updateCurrentCampaignSet(campaignSet: CampaignOverview[]){
+    if(this.backgroundImage == null) return;
+
+    this.allCampaignData = campaignSet;
+
+    const currentCampaignName: string = this.globalURLParamService.getURLCampaignParameter().value;
+    await this.updateCurrentCampaign(currentCampaignName);    
+  }
+
 
   findCampaignByName(campaignName: string): CampaignOverview{
     if (campaignName == null) return undefined;
@@ -61,5 +57,6 @@ export class BackgroundImageComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void{
     if (this.urlParamSubscription) this.urlParamSubscription.unsubscribe();
+    if (this.campaignSetSubscription) this.campaignSetSubscription.unsubscribe();
   }
 }
