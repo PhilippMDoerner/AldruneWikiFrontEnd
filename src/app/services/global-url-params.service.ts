344f3bcacc4af3f96a6c824dcdbd65cfb95c6bdc
Philipp Maria Doerner <philipp3000doerner@web.de>
2021-09-06 20:03:44 +0200

    Added a mechanism that allows me to also passively update the campaign set I have in the background, particularly if I just manipulated one of the campaigns

--- src/app/services/global-url-params.service.ts
+++ src/app/services/global-url-params.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { first } from 'rxjs/operators';
+import { CampaignOverview } from '../models/campaign';
+import { CampaignService } from './campaign.service';
 import { WarningsService } from './warnings.service';
 
 @Injectable({
@@ -7,17 +10,35 @@ import { WarningsService } from './warnings.service';
 })
 export class GlobalUrlParamsService {
   currentCampaignName: BehaviorSubject<string> = new BehaviorSubject(null);
+  currentCampaignSet: BehaviorSubject<CampaignOverview[]> = new BehaviorSubject(null); 
 
   constructor(
-  ) { }
+    campaignService: CampaignService,
+    warnings: WarningsService
+  ) { 
+    campaignService.campaignList().pipe(first()).subscribe(
+      (campaigns: CampaignOverview[]) => this.updateCampaignSet(campaigns),
+      error => warnings.showWarning(error)
+    );
+  }
 
   getURLCampaignParameter(): BehaviorSubject<string>{
     return this.currentCampaignName;
+  }
+
+  getCampaigns(): BehaviorSubject<CampaignOverview[]>{
+    return this.currentCampaignSet;
   }
 
   updateCampaignBackgroundImage(newCampaignName: string): void{
     if(newCampaignName === this.currentCampaignName.value) return;
 
     this.currentCampaignName.next(newCampaignName);
+  }
+
+  updateCampaignSet(campaigns: CampaignOverview[]): void{
+    console.log("Updating campaigns");
+    console.log(campaigns);
+    this.currentCampaignSet.next(campaigns);
   }
 }
