5837323f15d72ec21c9ca09c4dcb6f2fffe97665
Philipp Maria Doerner <philipp3000doerner@web.de>
2021-09-07 19:24:56 +0200

    Added animations to campaign overview coming in and sliding out

--- src/app/components/articles/campaign-overview/campaign-overview.component.ts
+++ src/app/components/articles/campaign-overview/campaign-overview.component.ts
@@ -1,31 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { first } from 'rxjs/operators';
 import { Constants } from 'src/app/app.constants';
 import { CampaignOverview } from 'src/app/models/campaign';
 import { CampaignService } from 'src/app/services/campaign.service';
+import { GlobalUrlParamsService } from 'src/app/services/global-url-params.service';
 import { RoutingService } from 'src/app/services/routing.service';
-import { WarningsService } from 'src/app/services/warnings.service';
+import { animateElement } from 'src/app/utils/functions/animationDecorator';
 
 @Component({
   selector: 'app-campaign-overview',
   templateUrl: './campaign-overview.component.html',
   styleUrls: ['./campaign-overview.component.scss']
 })
-export class CampaignOverviewComponent implements OnInit {
+export class CampaignOverviewComponent implements OnInit, AfterViewInit {
   campaignData: CampaignOverview[];
   constants: Constants = Constants;
 
+  @ViewChild('campaignOverviewContainer') campaignOverviewContainer: ElementRef;
+
+  globalUrlParamSubscription: Subscription;
+
   constructor(
-    private campaignService: CampaignService,
-    private warningService: WarningsService,
     public routingService: RoutingService,
+    private globalUrlParams: GlobalUrlParamsService,
   ) { }
 
-  ngOnInit(): void {
-    this.campaignService.campaignList().pipe(first()).subscribe(
-      (campaignData: CampaignOverview[]) => this.campaignData = campaignData,
-      error => this.routingService.routeToErrorPage(error)
+  async ngOnInit(): Promise<void> {
+    this.globalUrlParamSubscription = this.globalUrlParams.getCampaigns().subscribe(
+      (campaignData: CampaignOverview[]) => {
+        console.log("Starting to load overview");
+        this.campaignData = campaignData;
+      }
     );
   }
 
+  ngAfterViewInit(): void{
+    this.campaignOverviewContainer.nativeElement.style.setProperty('--animate-duration', '0.5s');
+    animateElement(this.campaignOverviewContainer.nativeElement, 'slideInDown');
+  }
+
+  routeToCampaign(campaignName: string ): void{
+    this.campaignOverviewContainer.nativeElement.style.setProperty('--animate-duration', '0.5s');
+    this.globalUrlParams.updateCampaignBackgroundImage(campaignName);
+
+    animateElement(this.campaignOverviewContainer.nativeElement, 'slideOutUp')
+      .then(() => this.routingService.routeToPath("home2", {campaign: campaignName}));
+  }
+
+  ngOnDestroy(): void{
+    if(this.globalUrlParamSubscription) this.globalUrlParamSubscription.unsubscribe();
+  }
 }
