ab697fc45b46d88b8f6bebce22894bcb907d5b7d
Philipp Maria Doerner <philipp3000doerner@web.de>
2021-09-10 14:14:39 +0200

    Full overhaul of article mixin
    Fundamentally changed how data loading is done.
    1) Ensure you have a campaignset
    2) Set up a subscription to current campaign that updates when current campaign updates (which will happen when the route is being updated)
    3) Within the campaignSubscription, set up a subscription to url parameters that updates article data when the route is being updated
    4) Setup surrounding code to load article data with a try-catch
    5) Load article data

--- src/app/utils/functions/articleMixin.ts
+++ src/app/utils/functions/articleMixin.ts
@@ -1,9 +1,10 @@
 import { Directive, OnDestroy, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Params } from "@angular/router";
 import { BehaviorSubject, Subscription } from "rxjs";
-import { first } from "rxjs/operators";
+import { filter, first } from "rxjs/operators";
 import { Constants } from "src/app/app.constants";
 import { ArticleObject } from "src/app/models/base-models";
+import { CampaignOverview } from "src/app/models/campaign";
 import { GenericObjectService } from "src/app/services/generic-object.service";
 import { GenericService } from "src/app/services/generic.service";
 import { GlobalUrlParamsService } from "src/app/services/global-url-params.service";
@@ -15,11 +16,13 @@ import { PermissionUtilityFunctionMixin } from "./permissionDecorators";
 export class ArticleMixin extends PermissionUtilityFunctionMixin implements OnInit, OnDestroy{
     constants = Constants;
     parameter_subscription: Subscription;
+    globalParamSubscription: Subscription;
 
-    queryParameterName: string; //Only use if the column for that parameter has the unique constraint, aka the parameter is unique
-    campaignParameterName: string;
+    queryParameterName: string = "name";
     articleData: ArticleObject;
-    campaign: string = this.route.snapshot.params.campaign;
+    isLoadingArticleData: boolean = false;
+
+    campaign: CampaignOverview;
 
     textfieldFormStateSubject: BehaviorSubject<string> = new BehaviorSubject(Constants.displayState);
 
@@ -30,21 +33,108 @@ export class ArticleMixin extends PermissionUtilityFunctionMixin implements OnIn
         public route: ActivatedRoute,
         public routingService: RoutingService,
         public warnings: WarningsService,
-        private globalUrlParams: GlobalUrlParamsService
+        public globalUrlParams: GlobalUrlParamsService
     ){
         super();
     }
 
+    /**
+     * The Callback Order:
+     * globalUrlParams.getCampaigns - Ensures through the filter on it that there must first be a campaign overview set. Sets up subscription so that campaign changes update the global value, but trigger no re-subscription to the route as that will be already set up the first time a campaign value comes around
+     *      --> afterBackgroundDataLoaded - Called whenever campaign changes. Ensures campaign is set and sets up the subscription to params to update the page whenever the params change
+     *      --> onArticleRouteChange - Called whenever the route changes. 
+     *      --> loadArticleData
+     */
     ngOnInit(): void{
-        this.parameter_subscription = this.route.params.subscribe( params => {
-            const queryParameter: string | number = params[this.queryParameterName];
+        this.globalUrlParams.getCampaigns()
+            .pipe(
+                filter((campaigns: CampaignOverview[]) => campaigns != null),
+                first()
+            )
+            .subscribe((_) => {
+                this.globalParamSubscription = this.globalUrlParams.getCurrentCampaign()
+                    .pipe(filter(campaign => campaign != null))
+                    .subscribe((campaign: CampaignOverview) => this.afterBackgroundDataLoaded(campaign));
+            });
 
-            this.articleService.readByParam(this.campaign, queryParameter).pipe(first()).subscribe(
-                (article: ArticleObject) => this.articleData = article, 
-                error => this.routingService.routeToErrorPage(error)
-            );        
-        });
+        this.onInitHook();
     }
+
+    /**
+     * @description Fired after it has been assured that campaignoverview set has been loaded. "this.campaign" is set within this callback
+     * as is the loading of article data. If you wish to work with campaign data, do it after this callback.
+     */
+    async afterBackgroundDataLoaded(campaign: CampaignOverview): Promise<void>{
+        this.campaign = campaign;
+
+        console.log("Got campaign, now loading url parameters")
+        console.log(this.campaign);
+
+        const parameterSubcsriptionNeedsToBeCreated = this.parameter_subscription == null;
+        if(!parameterSubcsriptionNeedsToBeCreated) return;
+        
+        this.parameter_subscription = this.route.params
+            .subscribe((params: Params) => this.onArticleRouteChange(this.campaign, params));
+    }
+
+    async onArticleRouteChange(campaign: CampaignOverview, params: Params){
+        console.log(`Setting up try catch for load article data while loading is ${this.isLoadingArticleData}`)
+        if(this.isLoadingArticleData) return;
+
+        this.isLoadingArticleData = true;
+
+        try{
+            await this.loadArticleData(campaign, params);
+        } catch(error) {
+            this.routingService.routeToErrorPage(error);
+        } finally{
+            this.isLoadingArticleData = false;
+        }
+    }
+
+    /**
+     * @description loads the data for the current article. Is fired either when the route changes
+     */
+    async loadArticleData(campaign: CampaignOverview, params: Params): Promise<void>{
+        console.log(`Attempting loading Article Data for campaign ${campaign.name} with param ${this.getQueryParameter(params)}`);
+        const campaignName: string = campaign.name;
+        if(campaignName == null) return;
+
+        const queryParameter: string | number = this.getQueryParameter(params);
+        if(queryParameter == null) return;
+
+        this.articleService.readByParam(campaignName, queryParameter)
+            .pipe(first())
+            .subscribe((articleData: ArticleObject) => this.articleData = articleData);
+    }
+
+    getQueryParameter(params: Params): string | number{
+        return params[this.queryParameterName];
+    }
+
+    /**
+     * @description A hook that executes every time the currently selected campaign changes.
+     * Executes after a request for new article data has been sent out
+     * @param params: The parameters of the current routes
+     */
+     afterArticleLoadHook(params: Params): void{}
+
+
+    /**
+     * @description A hook that executes every time the page finishes loading/reloading article data 
+     * Executes after the attribute "this.articleData" has been updated
+     * @param campaign: The currently selected campaign by the params
+     * @param params: The parameters of the current routes
+     */
+    onArticleLoadFinished(articleData: ArticleObject): void{
+        this.articleData = articleData;
+    }
+
+    /**
+     * @description A hook that executes after subscriptions have started for parameters and global parameters
+     * in ngOnInit()
+     */ 
+    onInitHook(): void{}
 
     onDescriptionUpdate(updatedDescription: string){
         const descriptionPatch = {description: updatedDescription, update_datetime: this.articleData.update_datetime};
@@ -80,5 +170,6 @@ export class ArticleMixin extends PermissionUtilityFunctionMixin implements OnIn
 
     ngOnDestroy(){
         if (this.parameter_subscription) this.parameter_subscription.unsubscribe();
+        if (this.globalParamSubscription) this.globalParamSubscription.unsubscribe();
     }
 }
