43c70bffa636fedb6099f6a852222f09dde9bd29
Philipp Maria Doerner <philipp3000doerner@web.de>
2021-09-11 11:47:50 +0200

    Added fade in animation to home page

--- src/app/components/home2/home2.component.ts
+++ src/app/components/home2/home2.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, HostBinding, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostBinding, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Constants } from 'src/app/app.constants';
@@ -6,14 +6,16 @@ import { CampaignOverview } from 'src/app/models/campaign';
 import { OverviewItem } from 'src/app/models/overviewItem';
 import { GlobalUrlParamsService } from 'src/app/services/global-url-params.service';
 import { RoutingService } from 'src/app/services/routing.service';
+import { animateElement } from 'src/app/utils/functions/animationDecorator';
 
 @Component({
   selector: 'app-home2',
   templateUrl: './home2.component.html',
   styleUrls: ['./home2.component.scss'],
 })
-export class Home2Component implements OnInit, OnDestroy {
+export class Home2Component implements OnInit, AfterViewInit, OnDestroy {
   @ViewChild("search") searchField: ElementRef;
+  @ViewChild('article') articleElement: ElementRef;
 
   searchString: string;
   constants = Constants;
@@ -34,6 +36,16 @@ export class Home2Component implements OnInit, OnDestroy {
     this.campaignSubscription = this.globalUrlParams.getCurrentCampaign().subscribe(
       (campaign: CampaignOverview) => this.campaignData = campaign
     );
+  }
+
+  ngAfterViewInit(): void{
+    this.animateHomePage();
+  }
+
+  animateHomePage(): void{
+    if(!this.articleElement?.nativeElement) return;
+
+    animateElement(this.articleElement.nativeElement, 'fadeIn');
   }
 
   sidebarColor(articleType: string): string{
