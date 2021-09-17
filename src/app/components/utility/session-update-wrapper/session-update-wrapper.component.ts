5642674639e55173b207fcc56cfd93b582ef08de
Philipp Maria Doerner <philipp3000doerner@web.de>
2021-09-17 11:50:02 +0200

    Made it so sessions are reloaded when you create, update or delete a session

--- src/app/components/utility/session-update-wrapper/session-update-wrapper.component.ts
+++ src/app/components/utility/session-update-wrapper/session-update-wrapper.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { FieldWrapper } from '@ngx-formly/core';
+import { CampaignOverview } from 'src/app/models/campaign';
 
 @Component({
   selector: 'app-session-update-wrapper',
@@ -7,5 +8,22 @@ import { FieldWrapper } from '@ngx-formly/core';
   styleUrls: ['./session-update-wrapper.component.scss']
 })
 
-export class SessionUpdateWrapperComponent extends FieldWrapper{}
+export class SessionUpdateWrapperComponent extends FieldWrapper{
+  campaign: CampaignOverview;
+  showField: boolean = true; //Needed to allow reinitializing the session field select options if its options have been updated
+
+  constructor(
+    private changeDetector: ChangeDetectorRef,
+  ){
+    super();
+  }
+
+  reinitializeSessionField(): void{
+    this.showField = false;
+    this.changeDetector.detectChanges();
+    this.showField = true;
+    this.changeDetector.detectChanges();
+  }
+
+}
 
