1fa32753ae7ba39ef2bcded6e78bf93814045db1
Philipp Maria Doerner <philipp3000doerner@web.de>
2021-10-23 19:31:57 +0200

    Removed unused imports

--- src/app/services/creature/creature.service.ts
+++ src/app/services/creature/creature.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Creature, CreatureObject } from "src/app/models/creature";
+import { CreatureObject } from "src/app/models/creature";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Constants } from "src/app/app.constants";
-import { Observable } from "rxjs";
-import { TransformArrayObservable, TransformObservable } from 'src/app/utils/functions/transform';
 import { GenericObjectService } from '../generic-object.service';
 
 @Injectable({
