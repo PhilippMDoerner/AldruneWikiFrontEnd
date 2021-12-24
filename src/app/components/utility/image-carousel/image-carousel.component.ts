f728987f3c956a62bb7550ed8a97b59238d47c11
Philipp Maria Doerner <philipp3000doerner@web.de>
2021-12-24 21:28:13 +0100

    Added loading indicator to image uploading, updating and deleting

--- src/app/components/utility/image-carousel/image-carousel.component.ts
+++ src/app/components/utility/image-carousel/image-carousel.component.ts
@@ -1,4 +1,10 @@
-import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  Input,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
 import { FormlyFieldConfig } from '@ngx-formly/core';
@@ -17,14 +23,15 @@ import { PermissionUtilityFunctionMixin } from 'src/app/utils/functions/permissi
 @Component({
   selector: 'app-image-carousel',
   templateUrl: './image-carousel.component.html',
-  styleUrls: ['./image-carousel.component.scss']
+  styleUrls: ['./image-carousel.component.scss'],
 })
 export class ImageCarouselComponent extends PermissionUtilityFunctionMixin {
   constants = Constants;
   FormState = FormState;
   formState: FormState = FormState.READ;
+  isLoading: boolean = false;
 
-  @Input() images : Image[];
+  @Input() images: Image[];
   @Input() article_type: string;
   @Input() article_pk: number;
 
@@ -34,46 +41,85 @@ export class ImageCarouselComponent extends PermissionUtilityFunctionMixin {
   serverModel: ImageObject;
 
   formlyFields: FormlyFieldConfig[] = [
-    this.formlyService.genericInput({key: "name", required: false}),
-    this.formlyService.genericInput({key: "character_article", label: "Character Article", hide: true, isNumberInput: true, required: false}),
-    this.formlyService.genericInput({key: "location_article", label: "Location Article", hide: true, isNumberInput: true, required: false}),
-    this.formlyService.genericInput({key: "creature_article", label: "Creature Article", hide: true, isNumberInput: true, required: false}),
-    this.formlyService.genericInput({key: "organization_article", label: "Organization Article", hide: true, isNumberInput: true, required: false}),
-    this.formlyService.genericInput({key: "encounter_article", label: "Encounter Article", hide: true, isNumberInput: true, required: false}),
-    this.formlyService.genericInput({key: "item_article", label: "Item Article", hide: true, isNumberInput: true, required: false}),
-    this.formlyService.singleFileField({key: "image"})
+    this.formlyService.genericInput({ key: 'name', required: false }),
+    this.formlyService.genericInput({
+      key: 'character_article',
+      label: 'Character Article',
+      hide: true,
+      isNumberInput: true,
+      required: false,
+    }),
+    this.formlyService.genericInput({
+      key: 'location_article',
+      label: 'Location Article',
+      hide: true,
+      isNumberInput: true,
+      required: false,
+    }),
+    this.formlyService.genericInput({
+      key: 'creature_article',
+      label: 'Creature Article',
+      hide: true,
+      isNumberInput: true,
+      required: false,
+    }),
+    this.formlyService.genericInput({
+      key: 'organization_article',
+      label: 'Organization Article',
+      hide: true,
+      isNumberInput: true,
+      required: false,
+    }),
+    this.formlyService.genericInput({
+      key: 'encounter_article',
+      label: 'Encounter Article',
+      hide: true,
+      isNumberInput: true,
+      required: false,
+    }),
+    this.formlyService.genericInput({
+      key: 'item_article',
+      label: 'Item Article',
+      hide: true,
+      isNumberInput: true,
+      required: false,
+    }),
+    this.formlyService.singleFileField({ key: 'image' }),
   ];
 
   constructor(
     private imageUploadService: ImageUploadService,
     private formlyService: MyFormlyService,
-    private warnings: WarningsService,  
+    private warnings: WarningsService,
     public routingService: RoutingService,
     route: ActivatedRoute,
-    tokenService: TokenService,
-  ) { 
+    tokenService: TokenService
+  ) {
     super(tokenService, route);
   }
 
-  getCurrentMainImage(): ImageObject{
+  getCurrentMainImage(): ImageObject {
     return this.images[this.currentImageIndex];
   }
 
-  onSlide(event){
+  onSlide(event) {
     const currentImageSlideId: string = event.current;
-    const currentImageIndexStr: string = currentImageSlideId.replace("imageIndex-", "");
+    const currentImageIndexStr: string = currentImageSlideId.replace(
+      'imageIndex-',
+      ''
+    );
     this.currentImageIndex = parseInt(currentImageIndexStr, 10);
   }
 
-  resetImageModel(): void{
+  resetImageModel(): void {
     this.userModel = new ImageObject();
   }
 
-  toggleDeleteState(): void{
+  toggleDeleteState(): void {
     this.formState = FormState.DELETE;
   }
 
-  toggleCreateState(): void{
+  toggleCreateState(): void {
     this.formState = FormState.CREATE;
 
     this.resetImageModel();
@@ -82,79 +128,103 @@ export class ImageCarouselComponent extends PermissionUtilityFunctionMixin {
     this.userModel[article_type_pk_key] = this.article_pk;
   }
 
-  toggleEditState(): void{
+  toggleEditState(): void {
     this.formState = FormState.UPDATE;
 
     const currentMainImage = this.images[this.currentImageIndex];
     this.userModel = currentMainImage;
   }
 
-  onCancel(): void{
+  onCancel(): void {
     this.formState = FormState.READ;
   }
 
-  onSubmit(): void{
-    if (this.formState === FormState.CREATE){
+  onSubmit(): void {
+    if (this.formState === FormState.CREATE) {
       this.createImage();
-    } else if (this.formState === FormState.UPDATE || this.formState === FormState.UPDATEOUTDATED){
+    } else if (
+      this.formState === FormState.UPDATE ||
+      this.formState === FormState.UPDATEOUTDATED
+    ) {
       this.updateImage();
     } else {
       throw `Submitted form while Image-Gallery Component was in invalid state ${this.formState}`;
     }
   }
 
-  updateImage(): void{
-    const isUpdateWithImageChange: boolean = this.userModel.image.constructor.name.toLowerCase() !== "string";
+  async updateImage(): Promise<void> {
+    this.isLoading = true;
+
+    const isUpdateWithImageChange: boolean =
+      this.userModel.image.constructor.name.toLowerCase() !== 'string';
 
     let updateObservable: Observable<ImageObject>;
-    if(isUpdateWithImageChange){
-      updateObservable = this.imageUploadService.update(this.userModel.pk, this.userModel);
-
+    if (isUpdateWithImageChange) {
+      updateObservable = this.imageUploadService.update(
+        this.userModel.pk,
+        this.userModel
+      );
     } else {
-      const minimizedUserModel = {pk: this.userModel.pk, name: this.userModel.name} as ImageObject;
-      updateObservable= this.imageUploadService.patch(this.userModel.pk, minimizedUserModel);
+      const minimizedUserModel = {
+        pk: this.userModel.pk,
+        name: this.userModel.name,
+      } as ImageObject;
+      updateObservable = this.imageUploadService.patch(
+        this.userModel.pk,
+        minimizedUserModel
+      );
     }
 
-    updateObservable
-      .pipe(first())
-      .subscribe(
-        (updatedImage: ImageObject) => {
-          updatedImage.image = updatedImage.image.replace(Constants.wikiUrl, ""); //Backend somehow returns ImageObject where ImageObject.image includes the wikiUrl, which it shouldn't
-          this.images[this.currentImageIndex] = updatedImage;
-          this.resetImageModel();
-  
-          this.formState = FormState.READ;
-        }, 
-        error => this.warnings.showWarning(error)
-      );
-  }
-
-  createImage(): void{
-    this.imageUploadService.create(this.userModel).pipe(first()).subscribe(
-      (createdImage: ImageObject) => {
-        this.images.push(createdImage);
+    updateObservable.pipe(first()).subscribe(
+      (updatedImage: ImageObject) => {
+        updatedImage.image = updatedImage.image.replace(Constants.wikiUrl, ''); //Backend somehow returns ImageObject where ImageObject.image includes the wikiUrl, which it shouldn't
+        this.images[this.currentImageIndex] = updatedImage;
         this.resetImageModel();
 
         this.formState = FormState.READ;
-      }, 
-      error => this.warnings.showWarning(error)
-    );  
+
+        this.isLoading = false;
+      },
+      (error) => this.warnings.showWarning(error)
+    );
   }
 
+  createImage(): void {
+    this.isLoading = true;
 
-  deleteCurrentMainImage(): void{
+    this.imageUploadService
+      .create(this.userModel)
+      .pipe(first())
+      .subscribe(
+        (createdImage: ImageObject) => {
+          this.images.push(createdImage);
+          this.resetImageModel();
+
+          this.formState = FormState.READ;
+          this.isLoading = false;
+        },
+        (error) => this.warnings.showWarning(error)
+      );
+  }
+
+  deleteCurrentMainImage(): void {
+    this.isLoading = true;
     const currentMainImage = this.getCurrentMainImage();
 
-    this.imageUploadService.delete(currentMainImage.pk).pipe(first()).subscribe(
-      response => {
-        this.images.splice(this.currentImageIndex, 1);
-        if (this.currentImageIndex === this.images.length){
-          this.currentImageIndex = this.images.length - 1;
-        }
+    this.imageUploadService
+      .delete(currentMainImage.pk)
+      .pipe(first())
+      .subscribe(
+        (response) => {
+          this.images.splice(this.currentImageIndex, 1);
+          if (this.currentImageIndex === this.images.length) {
+            this.currentImageIndex = this.images.length - 1;
+          }
 
-        this.formState = FormState.READ;
-      },
-      error => this.warnings.showWarning(error)
-    )
+          this.formState = FormState.READ;
+          this.isLoading = false;
+        },
+        (error) => this.warnings.showWarning(error)
+      );
   }
 }
