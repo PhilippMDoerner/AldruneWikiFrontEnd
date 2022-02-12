0c0334327017c5a1ba46a0f97e2aa4fbe10f31f9
PhilippMDoerner <philipp3000doerner@web.de>
2022-02-12 10:32:58 +0100

    Made minor adjustment to how marker text is shown

--- src/app/components/utility/leaflet-map/leaflet-map.component.ts
+++ src/app/components/utility/leaflet-map/leaflet-map.component.ts
@@ -1,10 +1,17 @@
-import { AfterContentInit, AfterViewInit, Component, Input, OnDestroy, OnInit } from '@angular/core';
+import {
+  AfterContentInit,
+  AfterViewInit,
+  Component,
+  Input,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 import { Router } from '@angular/router';
 import { Constants } from 'src/app/app.constants';
 import { ExtendedMap } from 'src/app/models/map';
 import { MapMarker, MapMarkerObject } from 'src/app/models/mapmarker';
 import { Marker } from 'leaflet';
-import * as L from "node_modules/leaflet";
+import * as L from 'node_modules/leaflet';
 import 'leaflet/dist/leaflet.css';
 import { RoutingService } from 'src/app/services/routing.service';
 import { CampaignOverview } from 'src/app/models/campaign';
@@ -12,65 +19,63 @@ import { CampaignOverview } from 'src/app/models/campaign';
 let DefaultIcon = L.icon({
   iconUrl: `${Constants.wikiMediaUrl}/leaflet/marker-icon.png`,
   shadowUrl: `${Constants.wikiMediaUrl}/leaflet/marker-shadow.png`,
-  iconSize: [24,36],
-  iconAnchor: [12,36]
+  iconSize: [24, 36],
+  iconAnchor: [12, 36],
 });
-
 
 @Component({
   selector: 'app-leaflet-map',
   templateUrl: './leaflet-map.component.html',
-  styleUrls: ['./leaflet-map.component.scss']
+  styleUrls: ['./leaflet-map.component.scss'],
 })
 export class LeafletMapComponent implements AfterContentInit {
   @Input() map: ExtendedMap;
   @Input() campaign: CampaignOverview;
 
   private leafletMap;
-  constructor(
-    private router: Router,  
-    public routingService: RoutingService,
-  ) { }
+  constructor(private router: Router, public routingService: RoutingService) {}
 
   mouseLatitude: number;
   mouseLongitude: number;
   hideCoordinatesState: boolean = true;
 
-
-  ngAfterContentInit(){
+  ngAfterContentInit() {
     /*
-     * It is mystifying why this is needed. If this isn't there, leaflet will mess up the initial 
+     * It is mystifying why this is needed. If this isn't there, leaflet will mess up the initial
      * calculation of the translate3D CSS rule of the .leaflet-image-layer element, which will cause the
-     * map to wildly jump around when clicked. Chances are, it's because translate3D is trying to be 
+     * map to wildly jump around when clicked. Chances are, it's because translate3D is trying to be
      * calculated before the map element is properly initialized and putting this onto another thread
      * forces it to wait for that.
      **/
     setTimeout(() => this.initMap(), 0);
   }
 
-  initMap(){
+  initMap() {
     this.leafletMap = L.map('leafletMapDiv', {
-        crs: L.CRS.Simple,
-        minZoom: -1,
-        maxZoom: 2,
+      crs: L.CRS.Simple,
+      minZoom: -1,
+      maxZoom: 2,
     });
     this.addMapImage();
     this.addMarkers();
     this.setMapEventListeners();
   }
 
-  setMapEventListeners(){
+  setMapEventListeners() {
     //Get the map coordinates of the point the mouse hovers over
-    this.leafletMap.on('mousemove', event => {
+    this.leafletMap.on('mousemove', (event) => {
       this.mouseLatitude = event.latlng.lat;
       this.mouseLongitude = event.latlng.lng;
     });
 
-    this.leafletMap.on('click', event => {
+    this.leafletMap.on('click', (event) => {
       const latitude = parseInt(event.latlng.lat);
       const longitude = parseInt(event.latlng.lng);
 
-      const popupContentHTML = this.makeFreePopupContentHTML(longitude, latitude);
+      const popupContentHTML = this.makeFreePopupContentHTML(
+        longitude,
+        latitude
+      );
       L.popup()
         .setLatLng([latitude, longitude])
         .setContent(popupContentHTML)
@@ -78,20 +83,26 @@ export class LeafletMapComponent implements AfterContentInit {
     });
   }
 
-  makeFreePopupContentHTML(longitude: number, latitude: number): string{
-    const markerMapCreateUrl: string = this.routingService.getRoutePath('marker-map-create', {
-      latitude: latitude,
-      longitude: longitude,
-      map_name: this.map.name,
-      campaign: this.campaign.name,
-    });
+  makeFreePopupContentHTML(longitude: number, latitude: number): string {
+    const markerMapCreateUrl: string = this.routingService.getRoutePath(
+      'marker-map-create',
+      {
+        latitude: latitude,
+        longitude: longitude,
+        map_name: this.map.name,
+        campaign: this.campaign.name,
+      }
+    );
 
-    const locationMapCreateUrl: string = this.routingService.getRoutePath('location-map-create', {
-      latitude: latitude, 
-      longitude: longitude, 
-      map_name: this.map.name,
-      campaign: this.campaign.name,
-    });
+    const locationMapCreateUrl: string = this.routingService.getRoutePath(
+      'location-map-create',
+      {
+        latitude: latitude,
+        longitude: longitude,
+        map_name: this.map.name,
+        campaign: this.campaign.name,
+      }
+    );
 
     const popupContentHTML: string = `
       <div class="mb-2 pointer"> 
@@ -110,38 +121,46 @@ export class LeafletMapComponent implements AfterContentInit {
     return popupContentHTML;
   }
 
-  addMapImage(): void{
-    const bounds: L.LatLngBoundsExpression = [[200, 140], [800, 1160]];
-    L.imageOverlay(this.map.image, bounds).addTo(this.leafletMap)
-    this.leafletMap.fitBounds(bounds);  
+  addMapImage(): void {
+    const bounds: L.LatLngBoundsExpression = [
+      [200, 140],
+      [800, 1160],
+    ];
+    L.imageOverlay(this.map.image, bounds).addTo(this.leafletMap);
+    this.leafletMap.fitBounds(bounds);
   }
 
-  addMarkers(): void{
+  addMarkers(): void {
     const layers = {};
 
     // Groups markers into their respective layers
-    for (let mapMarker of this.map.markers){
+    for (let mapMarker of this.map.markers) {
       const layerName = mapMarker.type_details.name;
-      const layer = (layers.hasOwnProperty(layerName)) ? layers[layerName] : L.layerGroup();  
-      layers[layerName] = layer;  
+      const layer = layers.hasOwnProperty(layerName)
+        ? layers[layerName]
+        : L.layerGroup();
+      layers[layerName] = layer;
 
-      const marker = (mapMarker.type_details.is_text_marker) ? this.createTextMarker(mapMarker) : this.createAwesomeMarker(mapMarker);
+      const marker = mapMarker.type_details.is_text_marker
+        ? this.createTextMarker(mapMarker)
+        : this.createAwesomeMarker(mapMarker);
       marker.addTo(layer);
     }
 
     // Adds every layer to leaflet map
-    for(const layerName in layers){
+    for (const layerName in layers) {
       layers[layerName].addTo(this.leafletMap);
     }
 
     // Add Layer-Control to Leaflet Map
-    L.control.layers(null, layers, {sortLayers: true}).addTo(this.leafletMap);
+    L.control.layers(null, layers, { sortLayers: true }).addTo(this.leafletMap);
   }
 
-
-  createTextMarker(mapMarker: MapMarkerObject): Marker{
+  createTextMarker(mapMarker: MapMarkerObject): Marker {
     const markerColor = this.getMarkerColor(mapMarker);
-    const textColor = (['beige', 'lightgreen'].includes(markerColor)) ? "black" : "white";
+    const textColor = ['beige', 'lightgreen'].includes(markerColor)
+      ? 'black'
+      : 'white';
     return L.marker([mapMarker.latitude, mapMarker.longitude], {
       icon: L.divIcon({
         html: `
@@ -149,23 +168,23 @@ export class LeafletMapComponent implements AfterContentInit {
           <strong>${mapMarker.location_details.name}</strong>
         </div>
         `,
-      })
+      }),
     })
-    .bindPopup(this.getPopupText(mapMarker))
-    .bindTooltip(mapMarker.location_details.name);
+      .bindPopup(this.getPopupText(mapMarker))
+      .bindTooltip(mapMarker.location_details.name);
   }
 
-  getMarkerColor(marker: MapMarker): string{
-    if (marker.color){
+  getMarkerColor(marker: MapMarker): string {
+    if (marker.color) {
       return marker.color;
-    } else if (marker.type_details.color){
+    } else if (marker.type_details.color) {
       return marker.type_details.color;
     } else {
-      return "grey";
+      return 'grey';
     }
   }
 
-  getPopupText(marker: MapMarker){
+  getPopupText(marker: MapMarker) {
     // Heading and Description
     const locationHeading: string = this.makeLocationHeading(marker);
     const locationDescription: string = this.makeLocationDescription(marker);
@@ -174,65 +193,80 @@ export class LeafletMapComponent implements AfterContentInit {
     return `${locationHeading} <br> ${locationDescription} <br> ${sublocationList}`;
   }
 
-  makeLocationHeading(marker: MapMarkerObject): string{
-    const location_url = this.routingService.getRoutePath("location", {
-      campaign: this.campaign.name, 
+  makeLocationHeading(marker: MapMarkerObject): string {
+    const location_url = this.routingService.getRoutePath('location', {
+      campaign: this.campaign.name,
       parent_name: marker.location_details.parent_location_name,
-      name: marker.location_details.name
+      name: marker.location_details.name,
     });
-    const heading: string = `<a href="${location_url}"> <b>${marker.location_details.name}</b> </a>`
+    const heading: string = `<a href="${location_url}"> <b>${marker.location_details.name}</b> </a>`;
     return heading;
   }
 
-  makeLocationDescription(marker: MapMarkerObject): string{
+  makeLocationDescription(marker: MapMarkerObject): string {
     let description: string = marker.location_details.description;
-    if (description && description.split(" ").length >= 35) description += "...";
+    if (description == null) {
+      return '';
+    }
+
+    let maxDescriptionWordCount = 35;
+    let descriptionTooLong: boolean =
+      description.split(' ').length >= maxDescriptionWordCount;
+    if (descriptionTooLong) {
+      let shortenedDescription = description
+        .split(' ')
+        .slice(0, maxDescriptionWordCount);
+      return `${shortenedDescription} ...`;
+    }
+
     return description;
   }
 
-  makeSublocationList(marker: MapMarkerObject): string{
-    if (marker.location_details.sublocations.length > 0){
-      const sublocationListHeading = '<h5 class="popup-heading"> Locations of Interest: </h5>';
+  makeSublocationList(marker: MapMarkerObject): string {
+    if (marker.location_details.sublocations.length > 0) {
+      const sublocationListHeading =
+        '<h5 class="popup-heading"> Locations of Interest: </h5>';
 
-      let sublocationList: string = " <ul>";
-      for(let sublocationName of marker.location_details.sublocations){
+      let sublocationList: string = ' <ul>';
+      for (let sublocationName of marker.location_details.sublocations) {
         const sublocationUrl = this.routingService.getRoutePath('location', {
           parent_name: marker.location_details.name,
           name: sublocationName,
           campaign: this.campaign.name,
         });
-        sublocationList += `<li><a href="${sublocationUrl}"> ${sublocationName}</a></li>`
+        sublocationList += `<li><a href="${sublocationUrl}"> ${sublocationName}</a></li>`;
       }
       sublocationList += '</ul>';
 
       return `${sublocationListHeading} ${sublocationList}`;
     }
-    return "";
+    return '';
   }
 
-  createAwesomeMarker(mapMarker: MapMarker): Marker{
-    return L.marker([mapMarker.latitude, mapMarker.longitude], {icon: this.divIcon(mapMarker)})
+  createAwesomeMarker(mapMarker: MapMarker): Marker {
+    return L.marker([mapMarker.latitude, mapMarker.longitude], {
+      icon: this.divIcon(mapMarker),
+    })
       .bindPopup(this.getPopupText(mapMarker))
       .bindTooltip(mapMarker.location_details.name);
   }
 
-  divIcon(mapMarker: MapMarker){
+  divIcon(mapMarker: MapMarker) {
     const typeIcon = mapMarker.type_details.icon;
     const fontawesome_type: string = mapMarker.type_details.fontawesome_type;
-    
+
     const typeColor = mapMarker.type_details.color;
     const customColor = mapMarker.color;
-    const color = customColor? customColor: typeColor;
+    const color = customColor ? customColor : typeColor;
 
     const markerIcon = L.divIcon({
       className: 'custom-div-icon',
       html: `<div style='background-color:${color};' class='marker-pin'></div><i class='d-flex justify-content-center ${fontawesome_type} fa-${typeIcon} awesome'>`,
       iconSize: [30, 42],
       iconAnchor: [15, 42],
-      color: color
+      color: color,
     });
 
     return markerIcon;
   }
-
 }
