ab7231374563b9dfe6efd336e02a873744ec9b76
Philipp Doerner <philipp3000doerner@web.de>
2022-06-12 21:47:51 +0200

    build update

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -4,10 +4,10 @@ export class Constants{
     public static get spaPrefix(): string { return "wiki2";}
 
     // API Urls
-    public static get wikiUrl(): string { return "https://www.aldrune.com";} //prod backend
-    public static get wikiApiUrl(): string { return `${this.wikiUrl}/${this.apiPrefix}/api`;} //prod backend
-    // public static get wikiUrl(): string { return "http://localhost:8000";} //dev backend
-    // public static get wikiApiUrl(): string { return `${this.wikiUrl}/api`;} //dev backend
+    // public static get wikiUrl(): string { return "https://www.aldrune.com";} //prod backend
+    public static get wikiUrl(): string { return "http://localhost:80";} //dev backend
+    public static get wikiApiUrl(): string { return `${this.wikiUrl}/${this.apiPrefix}/api`;} //prod backend / Docker container
+    // public static get wikiApiUrl(): string { return `${this.wikiUrl}/${this.apiPrefix}`;} //dev backend
     public static get wikiSPAUrl(): string{ return `${this.wikiUrl}/${this.spaPrefix}`;}
 
     public static get wikiStaticUrl(): string { return `${this.wikiUrl}/static`;}
