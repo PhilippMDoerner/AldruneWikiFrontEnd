3c9c0c133c4f7cf4732403410f1497f7846bc16e
Philipp Doerner <philipp3000doerner@web.de>
2022-07-15 20:45:47 +0200

    Added ability to deal with links

--- src/app/components/utility/inline-create-button/inline-create-button.component.ts
+++ src/app/components/utility/inline-create-button/inline-create-button.component.ts
@@ -1,12 +1,25 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+enum BadgeStyle{
+  Link,
+  Text
+}
+
 @Component({
   selector: 'app-inline-create-button',
   templateUrl: './inline-create-button.component.html',
   styleUrls: ['./inline-create-button.component.scss']
 })
-export class InlineCreateButtonComponent {
+export class InlineCreateButtonComponent implements OnInit{
   constructor() { }
-
+  
+  @Input() link: string;
   @Input() labelText: string;
+  badgeStyle: BadgeStyle;
+
+  styles = BadgeStyle;
+
+  ngOnInit(): void{
+    this.badgeStyle = this.link == null ? BadgeStyle.Text : BadgeStyle.Link;
+  }
 }
