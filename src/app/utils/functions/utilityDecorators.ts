73c4d2ced30633f0a5b3d2e9f55fd4732a2044d2
Philipp Doerner <philipp3000doerner@web.de>
2022-09-17 12:34:45 +0200

    ADD typing

--- src/app/utils/functions/utilityDecorators.ts
+++ src/app/utils/functions/utilityDecorators.ts
@@ -1,12 +1,15 @@
 import { Constants } from "src/app/app.constants";
   
-export function onlyOnTouchDevices(target: any, propertyKey: string, descriptor: PropertyDescriptor){
+export function onlyOnTouchDevices(target: any, propertyKey: string, descriptor: PropertyDescriptor): PropertyDescriptor{
   const originalMethod = descriptor.value;
-  descriptor.value = function(){
-      if (!Constants.isTouchDevice) return;
 
-      return originalMethod.apply(this, arguments);
-  }
+  const modifiedMethod = ()=>{
+    if (!Constants.isTouchDevice) return;
+
+    return originalMethod.apply(this, arguments);
+  };
+
+  descriptor.value = modifiedMethod;
 
   return descriptor;
 }
