ffae94e3bccd2e4dcf8aacd7cdd483bb73683b64
Philipp Doerner <philipp3000doerner@web.de>
2022-09-17 12:28:21 +0200

    Removed pointless stuff

--- src/app/utils/functions/utilityDecorators.ts
+++ src/app/utils/functions/utilityDecorators.ts
@@ -2,7 +2,6 @@ import { Constants } from "src/app/app.constants";
   
 export function onlyOnTouchDevices(target: any, propertyKey: string, descriptor: PropertyDescriptor){
   const originalMethod = descriptor.value;
-  console.log(Constants.isTouchDevice);
   descriptor.value = function(){
       if (!Constants.isTouchDevice) return;
 
