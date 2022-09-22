6fe8e97af3c2d24a261ec3dc9e4b2e9fda8ffecb
Philipp Doerner <philipp3000doerner@web.de>
2022-09-22 21:06:52 +0200

    FIX broken sidebar expansion

--- src/app/utils/functions/utilityDecorators.ts
+++ src/app/utils/functions/utilityDecorators.ts
@@ -3,14 +3,13 @@ import { Constants } from "src/app/app.constants";
 export function onlyOnTouchDevices(target: any, propertyKey: string, descriptor: PropertyDescriptor): PropertyDescriptor{
   const originalMethod = descriptor.value;
 
-  const modifiedMethod = ()=>{
+  const modifiedMethod = function(){
     if (!Constants.isTouchDevice) return;
 
     return originalMethod.apply(this, arguments);
-  };
+  }
 
   descriptor.value = modifiedMethod;
 
   return descriptor;
 }
-
