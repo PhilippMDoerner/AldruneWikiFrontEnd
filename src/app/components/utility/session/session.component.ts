f00ca93209b8aa11d92e1bb7041002dc9fe6138e
PhilippMDoerner <philipp3000doerner@web.de>
2024-08-24 08:39:06 +0200

    Fix session form

--- src/app/components/utility/session/session.component.ts
+++ src/app/components/utility/session/session.component.ts
@@ -1,8 +1,14 @@
-import { Component, ElementRef, EventEmitter, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { FormlyFieldConfig } from '@ngx-formly/core';
 import { CampaignOverview } from 'src/app/models/campaign';
-import { Session, SessionObject } from 'src/app/models/session';
+import { SessionObject } from 'src/app/models/session';
 import { MyFormlyService } from 'src/app/services/my-formly.service';
 import { RoutingService } from 'src/app/services/routing.service';
 import { SessionService } from 'src/app/services/session.service';
@@ -13,64 +19,88 @@ import { CardFormMixin } from 'src/app/utils/functions/cardMixin';
 @Component({
   selector: 'app-session',
   templateUrl: './session.component.html',
-  styleUrls: ['./session.component.scss']
+  styleUrls: ['./session.component.scss'],
 })
 export class SessionComponent extends CardFormMixin implements OnInit {
-
   diaryentryUrls: string[];
   sessionAudioUrl: string;
   userModelClass = SessionObject;
 
   @ViewChild('card') card: ElementRef;
 
-  cardDelete = new EventEmitter<number>()
+  cardDelete = new EventEmitter<number>();
 
   formlyFields: FormlyFieldConfig[] = [
-    this.formlyService.genericCheckbox({key: "is_main_session", defaultValue: true, label: "Main Session?"}),
-    this.formlyService.genericInput({key: "session_number", label: "Session Number", required: true, isNumberInput: true}),
-    this.formlyService.genericDatepicker({key: "session_date", label: "Date of the Session (YYYY/MM/DD)", required: true}),
-    this.formlyService.genericInput({key: "start_day", label: "Start Day", required: false}),
-    this.formlyService.genericInput({key: "end_day", label: "End Day", required: false})
+    this.formlyService.genericCheckbox({
+      key: 'is_main_session',
+      defaultValue: true,
+      label: 'Main Session?',
+    }),
+    this.formlyService.genericInput({
+      key: 'session_number',
+      label: 'Session Number',
+      required: true,
+      isNumberInput: true,
+    }),
+    this.formlyService.genericDatepicker({
+      key: 'session_date',
+      label: 'Date of the Session (YYYY/MM/DD)',
+      required: true,
+    }),
+    this.formlyService.genericInput({
+      key: 'start_day',
+      isNumberInput: true,
+      label: 'Start Day',
+      required: false,
+    }),
+    this.formlyService.genericInput({
+      key: 'end_day',
+      isNumberInput: true,
+      label: 'End Day',
+      required: false,
+    }),
   ];
 
   constructor(
     sessionService: SessionService,
     private formlyService: MyFormlyService,
-    warnings: WarningsService,  
+    warnings: WarningsService,
     private routingService: RoutingService,
     tokenService: TokenService,
     public element: ElementRef, //Allows calling this from the outside for scroll into view
     route: ActivatedRoute
-  ) { 
-    super(
-      warnings,
-      sessionService,
-      route,
-      tokenService
-    );
+  ) {
+    super(warnings, sessionService, route, tokenService);
   }
 
-  createUserModel(): void{
+  createUserModel(): void {
     this.userModel = JSON.parse(JSON.stringify(this.cardData));
   }
 
-  articleUpdate(userModel: any): void{
+  articleUpdate(userModel: any): void {
     userModel.end_day = parseInt(userModel.end_day);
     userModel.start_day = parseInt(userModel.start_day);
     super.articleUpdate(userModel);
   }
 
-  updateDynamicVariables(campaign: CampaignOverview, articleData: any, params: Params): void{
-    const diaryentries: {name: string, author_name: string}[] = articleData.diaryentries;
-    if(diaryentries != null){
-      this.diaryentryUrls = diaryentries.map(diaryentry => this.routingService.getRoutePath("diaryentry", {
-        authorName: diaryentry.author_name,
-        campaign: campaign.name,
-        sessionNumber: this.cardData.session_number,
-        isMainSession: this.cardData.is_main_session_int,
-      }));
+  updateDynamicVariables(
+    campaign: CampaignOverview,
+    articleData: any,
+    params: Params
+  ): void {
+    const diaryentries: { name: string; author_name: string }[] =
+      articleData.diaryentries;
+    if (diaryentries != null) {
+      this.diaryentryUrls = diaryentries.map((diaryentry) =>
+        this.routingService.getRoutePath('diaryentry', {
+          authorName: diaryentry.author_name,
+          campaign: campaign.name,
+          sessionNumber: this.cardData.session_number,
+          isMainSession: this.cardData.is_main_session_int,
+        })
+      );
 
-      this.sessionAudioUrl = this.routingService.getRoutePath("sessionaudio", {
+      this.sessionAudioUrl = this.routingService.getRoutePath('sessionaudio', {
         campaign: campaign.name,
         isMainSession: this.cardData.is_main_session_int,
         sessionNumber: this.cardData.session_number,
