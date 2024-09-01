94d85367ec320753a54451c4c4b50ba6df3871b2
PhilippMDoerner <philipp3000doerner@web.de>
2024-09-01 11:11:51 +0200

    build update

--- src/app/components/articles/campaign-update/campaign-update.component.ts
+++ src/app/components/articles/campaign-update/campaign-update.component.ts
@@ -3,7 +3,8 @@ import { ActivatedRoute, Params, Router } from '@angular/router';
 import { FormlyFieldConfig } from '@ngx-formly/core';
 import { first } from 'rxjs/operators';
 import { OverviewType } from 'src/app/app.constants';
-import { CampaignObject, CampaignOverview } from 'src/app/models/campaign';
+import { ApiObject } from 'src/app/models/base-models';
+import { CampaignObject } from 'src/app/models/campaign';
 import { CampaignService } from 'src/app/services/campaign.service';
 import { GlobalUrlParamsService } from 'src/app/services/global-url-params.service';
 import { MyFormlyService } from 'src/app/services/my-formly.service';
@@ -15,7 +16,7 @@ import { ArticleFormMixin } from 'src/app/utils/functions/articleFormMixin';
 @Component({
   selector: 'app-campaign-update',
   templateUrl: './campaign-update.component.html',
-  styleUrls: ['./campaign-update.component.scss']
+  styleUrls: ['./campaign-update.component.scss'],
 })
 export class CampaignUpdateComponent extends ArticleFormMixin {
   //URLs
@@ -26,16 +27,36 @@ export class CampaignUpdateComponent extends ArticleFormMixin {
   serverModel: CampaignObject;
   userModel: any;
   userModelClass = CampaignObject;
-  
-  updateCancelRoute = {routeName: "home2", params: {campaign: null}};
-  creationCancelRoute = {routeName: "campaign-overview", params: {}};
+
+  updateCancelRoute = { routeName: 'home2', params: { campaign: null } };
+  creationCancelRoute = { routeName: 'campaign-overview', params: {} };
 
   formlyFields: FormlyFieldConfig[] = [
-    this.formlyService.genericInput({key: "name", isNameInput: true, required: true, maxLength: 400}),
-    this.formlyService.genericInput({key: "subtitle", isNameInput: false, required: false, maxLength: 400}),
-    this.formlyService.genericSelect({key: "default_map", label: "Default Map", valueProp: "pk", overviewType: OverviewType.Map, campaign: this.campaign.name, required: false}),
-    this.formlyService.singleFileField({key: "background_image", required: true}),
-    this.formlyService.singleFileField({key: "icon", required: true}),
+    this.formlyService.genericInput({
+      key: 'name',
+      isNameInput: true,
+      required: true,
+      maxLength: 400,
+    }),
+    this.formlyService.genericInput({
+      key: 'subtitle',
+      isNameInput: false,
+      required: false,
+      maxLength: 400,
+    }),
+    this.formlyService.genericSelect({
+      key: 'default_map',
+      label: 'Default Map',
+      valueProp: 'pk',
+      overviewType: OverviewType.Map,
+      campaign: this.campaign.name,
+      required: false,
+    }),
+    this.formlyService.singleFileField({
+      key: 'background_image',
+      required: true,
+    }),
+    this.formlyService.singleFileField({ key: 'icon', required: true }),
   ];
 
   constructor(
@@ -46,7 +67,7 @@ export class CampaignUpdateComponent extends ArticleFormMixin {
     campaignService: CampaignService,
     route: ActivatedRoute,
     globalUrlParam: GlobalUrlParamsService,
-    tokenService: TokenService,
+    tokenService: TokenService
   ) {
     super(
       router,
@@ -56,27 +77,53 @@ export class CampaignUpdateComponent extends ArticleFormMixin {
       campaignService,
       globalUrlParam,
       route,
-      tokenService,
-    )
+      tokenService
+    );
   }
 
-  getQueryParameters(params: Params): object{
-    return {name: params.campaign};
+  getQueryParameters(params: Params): object {
+    return { name: params.campaign };
   }
 
-  updateRouterLinks(campaignName: string, userModel: CampaignObject, params: Params): void{
-    this.campaignAdminUrl = this.routingService.getRoutePath('campaign-admin', {campaign: params.campaign});
-    this.campaignOverviewUrl = this.routingService.getRoutePath('campaign-overview');
+  updateRouterLinks(
+    campaignName: string,
+    userModel: CampaignObject,
+    params: Params
+  ): void {
+    this.campaignAdminUrl = this.routingService.getRoutePath('campaign-admin', {
+      campaign: params.campaign,
+    });
+    this.campaignOverviewUrl =
+      this.routingService.getRoutePath('campaign-overview');
   }
 
-  onUpdateSuccess(updatedArticle: any, self: any){
-    this.routingService.routeToPath('campaign-admin', {campaign: updatedArticle.name});
+  onUpdateSuccess(updatedArticle: any, self: any) {
+    this.routingService.routeToPath('campaign-admin', {
+      campaign: updatedArticle.name,
+    });
 
     this.globalUrlParam.autoUpdateCampaignSet();
   }
 
-  async onCreationSuccess(createdArticle: any, self: ArticleFormMixin){
-    throw "InvalidFunctionError. Campaign Update can not create campaigns";
+  articleUpdate(userModel: any) {
+    if (userModel.pk == null)
+      throw `Invalid userModel Exception. The userModel provided here does not have a primary
+    key defined. This primary key is necessary to update the correct article in the database. Please contact the developer`;
+
+    const executionContext = this;
+
+    this.articleService
+      .patch(userModel.pk, userModel)
+      .pipe(first())
+      .subscribe(
+        (response: ApiObject) =>
+          this.onUpdateSuccess(response, executionContext),
+        (errorResponse: any) =>
+          this.onUpdateError(errorResponse, executionContext)
+      );
   }
 
+  async onCreationSuccess(createdArticle: any, self: ArticleFormMixin) {
+    throw 'InvalidFunctionError. Campaign Update can not create campaigns';
+  }
 }
--- src/app/services/campaign.service.ts
+++ src/app/services/campaign.service.ts
@@ -2,148 +2,203 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { EmptySearchResponse } from 'src/app/models/emptySearchResponse';
 import { Constants } from '../app.constants';
-import {Campaign, CampaignOverview, WikiStatistics } from '../models/campaign';
+import {
+  BaseCampaignData,
+  Campaign,
+  CampaignOverview,
+  WikiStatistics,
+} from '../models/campaign';
 import { User } from '../models/user';
-import { convertMultiFileModelToFormData, convertSingleFileModelToFormData } from '../utils/formDataConverter';
+import {
+  convertMultiFileModelToFormData,
+  convertSingleFileModelToFormData,
+} from '../utils/formDataConverter';
 import { GenericService } from './generic.service';
 import { TokenService } from './token.service';
-import { EmptySearchResponse } from 'src/app/models/emptySearchResponse';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class CampaignService extends GenericService{
+export class CampaignService extends GenericService {
   baseUrl: string = `${Constants.wikiApiUrl}/campaign`;
 
-  constructor(
-    http : HttpClient,
-    private tokenService: TokenService,
-  ) { 
-    super(http)  //Second param indicates which class the data of this service is turned into
+  constructor(http: HttpClient, private tokenService: TokenService) {
+    super(http); //Second param indicates which class the data of this service is turned into
   }
 
-
-
-  campaignList(): Observable<CampaignOverview[]>{
-    const campaignObs: Observable<CampaignOverview[]> =  this.http.get<CampaignOverview[]>(`${this.baseUrl}/overview/`);
+  campaignList(): Observable<CampaignOverview[]> {
+    const campaignObs: Observable<CampaignOverview[]> = this.http.get<
+      CampaignOverview[]
+    >(`${this.baseUrl}/overview/`);
     return campaignObs.pipe(
       map((campaigns: CampaignOverview[]) => {
-        campaigns.forEach(campaign => {
-          campaign.isMember = this.tokenService.isCampaignMember(campaign.name.toLowerCase());
-          campaign.isAdmin = this.tokenService.isCampaignAdmin(campaign.name.toLowerCase());
-          campaign.isGuest = this.tokenService.isCampaignGuest(campaign.name.toLowerCase());
+        campaigns.forEach((campaign) => {
+          campaign.isMember = this.tokenService.isCampaignMember(
+            campaign.name.toLowerCase()
+          );
+          campaign.isAdmin = this.tokenService.isCampaignAdmin(
+            campaign.name.toLowerCase()
+          );
+          campaign.isGuest = this.tokenService.isCampaignGuest(
+            campaign.name.toLowerCase()
+          );
         });
         return campaigns;
       })
     );
   }
 
-  create(data: Campaign): Observable<Campaign>{
+  create(data: Campaign): Observable<Campaign> {
     const campaignData = this.processCampaignData(data);
     return super.create(campaignData);
   }
 
-  update(pk: number, data: Campaign): Observable<Campaign>{
+  update(pk: number, data: Campaign): Observable<Campaign> {
     const campaignData = this.processCampaignData(data);
     return super.update(pk, campaignData);
   }
 
-  patch(pk: number, data: Campaign): Observable<Campaign>{
-    const campaignData = this.processCampaignData(data);
-    return super.patch(pk, campaignData);
+  patch(pk: number, data: Campaign): Observable<Campaign> {
+    const campaign: Omit<
+      BaseCampaignData,
+      'icon' | 'background_image' | 'pk' | 'default_map_details'
+    > = {
+      name: data.name,
+      subtitle: data.subtitle,
+      default_map: data.default_map,
+      has_audio_recording_permission: data.has_audio_recording_permission,
+      is_deactivated: data.is_deactivated,
+    };
+    return super.patch(pk, campaign);
   }
 
-  private processCampaignData(userModel: Campaign): Campaign | FormData{
+  private processCampaignData(userModel: Campaign): Campaign | FormData {
     const hasNewIcon: boolean = this.hasImageSelected(userModel.icon);
-    const hasNewBackgroundImage: boolean = this.hasImageSelected(userModel.background_image);
+    const hasNewBackgroundImage: boolean = this.hasImageSelected(
+      userModel.background_image
+    );
 
-    if (!hasNewIcon && !hasNewBackgroundImage){
+    if (!hasNewIcon && !hasNewBackgroundImage) {
       delete userModel.icon;
       delete userModel.background_image;
       return userModel;
-
-    } else if (!hasNewIcon && hasNewBackgroundImage){
+    } else if (!hasNewIcon && hasNewBackgroundImage) {
       delete userModel.icon;
-      const userModelFormData: FormData = convertSingleFileModelToFormData(userModel, 'background_image');
+      const userModelFormData: FormData = convertSingleFileModelToFormData(
+        userModel,
+        'background_image'
+      );
       return userModelFormData;
-
-    } else if (hasNewIcon && !hasNewBackgroundImage){
+    } else if (hasNewIcon && !hasNewBackgroundImage) {
       delete userModel.background_image;
-      const userModelFormData: FormData = convertSingleFileModelToFormData(userModel, 'icon');
+      const userModelFormData: FormData = convertSingleFileModelToFormData(
+        userModel,
+        'icon'
+      );
       return userModelFormData;
-
     } else {
-      const userModelFormData: FormData = convertMultiFileModelToFormData(userModel, ["background_image", "icon"]);
+      const userModelFormData: FormData = convertMultiFileModelToFormData(
+        userModel,
+        ['background_image', 'icon']
+      );
       return userModelFormData;
     }
   }
 
-  private hasImageSelected(imageFieldValue: any) : boolean{
-    return imageFieldValue.constructor.name === "FileList";
+  private hasImageSelected(imageFieldValue: any): boolean {
+    return imageFieldValue.constructor.name === 'FileList';
   }
 
-  delete(pk: number): Observable<any>{
+  delete(pk: number): Observable<any> {
     throw "You can not delete a campaign, please use 'deactivate' instead";
   }
 
-  /** Under the hood this may call "delete" but "delete" does not actually delete a campaign in the backend, it just deactivates it 
+  /** Under the hood this may call "delete" but "delete" does not actually delete a campaign in the backend, it just deactivates it
    * The functions were renamed to make that fact clear
    */
-  deactivate(pk: number): Observable<any>{
+  deactivate(pk: number): Observable<any> {
     return super.delete(pk);
   }
 
-  statistics(campaign_name: string): Observable<any>{
-    return this.http.get<WikiStatistics>(`${Constants.wikiApiUrl}/admin/statistics/${campaign_name}`);
+  statistics(campaign_name: string): Observable<any> {
+    return this.http.get<WikiStatistics>(
+      `${Constants.wikiApiUrl}/admin/statistics/${campaign_name}`
+    );
   }
 
   /**
    * @description Allows you to send a read query based on a param, e.g. "name", assuming the backend is set up for it.
    * The targetted URL will be "${baseURL of API Endpoint}/param"
-   * @param param 
+   * @param param
    * @returns The data from that endpoint by the service
    */
-  readByParam(campaign: string): Observable<any>{
+  readByParam(campaign: string): Observable<any> {
     return this.http.get(`${this.baseUrl}/${campaign}/`);
   }
 
-  addGuest(campaign: string, user: User): Observable<User[]>{
-    const requestBody = {action: "add_guest", user}
-    return this.http.patch<User[]>(`${this.baseUrl}/${campaign}/members/`, requestBody);
+  addGuest(campaign: string, user: User): Observable<User[]> {
+    const requestBody = { action: 'add_guest', user };
+    return this.http.patch<User[]>(
+      `${this.baseUrl}/${campaign}/members/`,
+      requestBody
+    );
   }
 
-  addMember(campaign: string, user: User): Observable<User[]>{
-    const requestBody = {action: "add_member", user}
-    return this.http.patch<User[]>(`${this.baseUrl}/${campaign}/members/`, requestBody);
+  addMember(campaign: string, user: User): Observable<User[]> {
+    const requestBody = { action: 'add_member', user };
+    return this.http.patch<User[]>(
+      `${this.baseUrl}/${campaign}/members/`,
+      requestBody
+    );
   }
 
-  addAdmin(campaign: string, user: User): Observable<User[]>{
-    const requestBody = {action: "add_admin", user}
+  addAdmin(campaign: string, user: User): Observable<User[]> {
+    const requestBody = { action: 'add_admin', user };
 
-    return this.http.patch<User[]>(`${this.baseUrl}/${campaign}/members/`, requestBody);
+    return this.http.patch<User[]>(
+      `${this.baseUrl}/${campaign}/members/`,
+      requestBody
+    );
   }
 
-  removeGuest(campaign: string, user: User): Observable<User[]>{
-    const requestBody = {action: "remove_guest", user}
-    return this.http.patch<User[]>(`${this.baseUrl}/${campaign}/members/`, requestBody);
+  removeGuest(campaign: string, user: User): Observable<User[]> {
+    const requestBody = { action: 'remove_guest', user };
+    return this.http.patch<User[]>(
+      `${this.baseUrl}/${campaign}/members/`,
+      requestBody
+    );
   }
 
-  removeMember(campaign: string, user: User): Observable<User[]>{
-    const requestBody = {action: "remove_member", user}
-    return this.http.patch<User[]>(`${this.baseUrl}/${campaign}/members/`, requestBody);
+  removeMember(campaign: string, user: User): Observable<User[]> {
+    const requestBody = { action: 'remove_member', user };
+    return this.http.patch<User[]>(
+      `${this.baseUrl}/${campaign}/members/`,
+      requestBody
+    );
   }
 
-  removeAdmin(campaign: string, user: User): Observable<User[]>{
-    const requestBody = {action: "remove_admin", user}
-    return this.http.patch<User[]>(`${this.baseUrl}/${campaign}/members/`, requestBody);
+  removeAdmin(campaign: string, user: User): Observable<User[]> {
+    const requestBody = { action: 'remove_admin', user };
+    return this.http.patch<User[]>(
+      `${this.baseUrl}/${campaign}/members/`,
+      requestBody
+    );
   }
 
-  addEmptySearchResponse(responseModel: EmptySearchResponse): Observable<EmptySearchResponse>{
-    return this.http.post<EmptySearchResponse>(`${Constants.wikiApiUrl}/emptysearchresponse/`, responseModel);
+  addEmptySearchResponse(
+    responseModel: EmptySearchResponse
+  ): Observable<EmptySearchResponse> {
+    return this.http.post<EmptySearchResponse>(
+      `${Constants.wikiApiUrl}/emptysearchresponse/`,
+      responseModel
+    );
   }
 
-  deleteEmptySearchResponse(emptySearchResponsePk: number): Observable<any>{
-    return this.http.delete(`${Constants.wikiApiUrl}/emptysearchresponse/pk/${emptySearchResponsePk}`);
+  deleteEmptySearchResponse(emptySearchResponsePk: number): Observable<any> {
+    return this.http.delete(
+      `${Constants.wikiApiUrl}/emptysearchresponse/pk/${emptySearchResponsePk}`
+    );
   }
 }
