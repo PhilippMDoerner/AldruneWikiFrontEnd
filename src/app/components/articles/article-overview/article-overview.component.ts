783d7d34121344a3ee34b3e8666159d609bfa8cc
PhilippMDoerner <philipp3000doerner@web.de>
2024-09-14 11:32:14 +0200

    Add autofocus

--- src/app/components/articles/article-overview/article-overview.component.ts
+++ src/app/components/articles/article-overview/article-overview.component.ts
@@ -1,31 +1,40 @@
-import { Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Constants, OverviewType } from 'src/app/app.constants';
 import { CampaignOverview } from 'src/app/models/campaign';
-import { OverviewItemObject } from "src/app/models/overviewItem";
+import { OverviewItemObject } from 'src/app/models/overviewItem';
 import { RoutingService } from 'src/app/services/routing.service';
 import { TokenService } from 'src/app/services/token.service';
 import { animateElement } from 'src/app/utils/functions/animationDecorator';
 import { PermissionUtilityFunctionMixin } from 'src/app/utils/functions/permissionDecorators';
 
-interface OverviewMetaData{
-  image: string,
-  heading: string,
-  processing: (listItems: OverviewItemObject[]) => OverviewItemObject[],
-  overviewTypeEnum: OverviewType,
+interface OverviewMetaData {
+  image: string;
+  heading: string;
+  processing: (listItems: OverviewItemObject[]) => OverviewItemObject[];
+  overviewTypeEnum: OverviewType;
 }
 interface OverviewMetaDataMap {
-  [key: string]: OverviewMetaData
+  [key: string]: OverviewMetaData;
 }
-
-
 
 @Component({
   selector: 'app-article-overview',
   templateUrl: './article-overview.component.html',
   styleUrls: ['./article-overview.component.scss'],
 })
-export class ArticleOverviewComponent extends PermissionUtilityFunctionMixin implements OnInit {
+export class ArticleOverviewComponent
+  extends PermissionUtilityFunctionMixin
+  implements OnInit, AfterViewInit
+{
   OverviewType = OverviewType;
 
   //URLS
@@ -39,116 +48,134 @@ export class ArticleOverviewComponent extends PermissionUtilityFunctionMixin imp
   isInitialAnimationFinished: boolean = false;
   campaign: CampaignOverview;
 
-  @ViewChildren("filter") filterField: QueryList<ElementRef>;
+  @ViewChildren('filter') filterField: QueryList<ElementRef>;
   @ViewChild('overviewMainCard') articleElement: ElementRef;
 
   constants: any = Constants;
   overviewTypeMetaData: OverviewMetaDataMap = {
     character: {
       image: `/assets/overview_images/characters.webp`,
-      heading: "Characters",
-      processing: (listItems: OverviewItemObject[]) => this.processCharacterOverviewItems(listItems),
-      overviewTypeEnum: OverviewType.Character
+      heading: 'Characters',
+      processing: (listItems: OverviewItemObject[]) =>
+        this.processCharacterOverviewItems(listItems),
+      overviewTypeEnum: OverviewType.Character,
     },
     creature: {
       image: `/assets/overview_images/creatures.webp`,
-      heading: "Creatures",
+      heading: 'Creatures',
       processing: null,
-      overviewTypeEnum: OverviewType.Creature
+      overviewTypeEnum: OverviewType.Creature,
     },
     diaryentry: {
       image: `/assets/overview_images/diaryentries.webp`,
-      heading: "Diaryentries",
-      processing: (listItems: OverviewItemObject[]) => this.processDiaryentryOverviewItems(listItems),
-      overviewTypeEnum: OverviewType.Diaryentry
+      heading: 'Diaryentries',
+      processing: (listItems: OverviewItemObject[]) =>
+        this.processDiaryentryOverviewItems(listItems),
+      overviewTypeEnum: OverviewType.Diaryentry,
     },
     item: {
       image: `/assets/overview_images/items.webp`,
-      heading: "Items",
+      heading: 'Items',
       processing: null,
-      overviewTypeEnum: OverviewType.Item
+      overviewTypeEnum: OverviewType.Item,
     },
     location: {
       image: `/assets/overview_images/locations.webp`,
-      heading: "Locations",
-      processing: (listItems: OverviewItemObject[]) => this.processLocationOverviewItems(listItems),
-      overviewTypeEnum: OverviewType.Location
+      heading: 'Locations',
+      processing: (listItems: OverviewItemObject[]) =>
+        this.processLocationOverviewItems(listItems),
+      overviewTypeEnum: OverviewType.Location,
     },
     organization: {
       image: `/assets/overview_images/organizations.webp`,
-      heading: "Organizations",
+      heading: 'Organizations',
       processing: null,
-      overviewTypeEnum: OverviewType.Organization
+      overviewTypeEnum: OverviewType.Organization,
     },
-  }
+  };
 
   overviewTypeName: string;
 
-  characterOverview: string = "character";
-  itemOverview: string = "item";
-  creatureOverview: string = "creature";
-  locationOverview: string = "location";
-  organizationOverview: string = "organization";
-  diaryentryOverview: string = "diaryentry";
+  characterOverview: string = 'character';
+  itemOverview: string = 'item';
+  creatureOverview: string = 'creature';
+  locationOverview: string = 'location';
+  organizationOverview: string = 'organization';
+  diaryentryOverview: string = 'diaryentry';
 
   constructor(
-    private router: Router,  
+    private router: Router,
     public routingService: RoutingService,
     route: ActivatedRoute,
-    tokenService: TokenService,
-  ) { 
+    tokenService: TokenService
+  ) {
     super(tokenService, route);
   }
 
   ngOnInit(): void {
-    this.campaign = this.route.snapshot.data["campaign"];
+    this.campaign = this.route.snapshot.data['campaign'];
 
-    const listItems: OverviewItemObject[] = this.route.snapshot.data["overviewList"];
+    const listItems: OverviewItemObject[] =
+      this.route.snapshot.data['overviewList'];
     const configs = this.getOverviewConfigs();
     const processingFunction = configs.processing;
     const hasProcessingFunction = processingFunction != null;
-    this.listItems = hasProcessingFunction ? processingFunction(listItems) : listItems;
+    this.listItems = hasProcessingFunction
+      ? processingFunction(listItems)
+      : listItems;
 
     this.setRouterLinks(this.campaign, this.listItems);
   }
 
-  setRouterLinks(campaign: CampaignOverview, listItems: OverviewItemObject[]): void{
-    this.homeUrl = this.routingService.getRoutePath('home2', {campaign: campaign.name});
+  setRouterLinks(
+    campaign: CampaignOverview,
+    listItems: OverviewItemObject[]
+  ): void {
+    this.homeUrl = this.routingService.getRoutePath('home2', {
+      campaign: campaign.name,
+    });
   }
 
-  getOverviewConfigs(): OverviewMetaData{
+  getOverviewConfigs(): OverviewMetaData {
     const urlSplit: string[] = this.router.url.split('/');
     this.overviewTypeName = urlSplit[urlSplit.length - 2];
-    const configs: OverviewMetaData = this.overviewTypeMetaData[this.overviewTypeName];
+    const configs: OverviewMetaData =
+      this.overviewTypeMetaData[this.overviewTypeName];
     return configs;
   }
 
-  ngAfterViewInit(): void{
-    if(!this.articleElement?.nativeElement) return;
+  ngAfterViewInit(): void {
+    if (!this.articleElement?.nativeElement) return;
 
     animateElement(this.articleElement.nativeElement, 'fadeIn');
+    this.focusFilterField();
   }
 
   //This function exists only so that it triggers the NgClass of the listItems in the template
-  filterListItems(){}
+  filterListItems() {}
 
-  openFirstArticle(filterValue: string){
-    const filterValueLower:string = filterValue.toLowerCase();
-  
+  openFirstArticle(filterValue: string) {
+    const filterValueLower: string = filterValue.toLowerCase();
+
     const filteredListItems: OverviewItemObject[] = this.listItems.filter(
-      (item: OverviewItemObject) => item.name.toLowerCase().includes(filterValueLower)
+      (item: OverviewItemObject) =>
+        item.name.toLowerCase().includes(filterValueLower)
     );
 
     const firstFilteredListItem = filteredListItems[0];
     this.routingService.routeToApiObject(firstFilteredListItem);
   }
 
-  processCharacterOverviewItems(characterListItems: OverviewItemObject[]): OverviewItemObject[]{
+  processCharacterOverviewItems(
+    characterListItems: OverviewItemObject[]
+  ): OverviewItemObject[] {
     const playerCharacters: OverviewItemObject[] = [];
     const nonPlayerCharacters: OverviewItemObject[] = [];
 
-    characterListItems.forEach(
-      (item: OverviewItemObject) => item.player_character ? playerCharacters.push(item) : nonPlayerCharacters.push(item)
+    characterListItems.forEach((item: OverviewItemObject) =>
+      item.player_character
+        ? playerCharacters.push(item)
+        : nonPlayerCharacters.push(item)
     );
 
     this.playerCharacters = playerCharacters;
@@ -156,41 +183,57 @@ export class ArticleOverviewComponent extends PermissionUtilityFunctionMixin imp
     return nonPlayerCharacters;
   }
 
-  processDiaryentryOverviewItems(diaryentryListItems: OverviewItemObject[]): OverviewItemObject[]{
-    diaryentryListItems.forEach( 
-      (item: OverviewItemObject) => item.name_full = this.buildDiaryEntryNameForList(item)
+  processDiaryentryOverviewItems(
+    diaryentryListItems: OverviewItemObject[]
+  ): OverviewItemObject[] {
+    diaryentryListItems.forEach(
+      (item: OverviewItemObject) =>
+        (item.name_full = this.buildDiaryEntryNameForList(item))
     );
     return diaryentryListItems;
   }
-  
+
   /**
-   * @description Generates the full name of a location based on its path in the location tree, concatenating 
+   * @description Generates the full name of a location based on its path in the location tree, concatenating
    * any parent name at the start.
    * @param {this} context - This component, needed to grant access despite the function being assigned to an object.
    */
-  processLocationOverviewItems(locationItems: OverviewItemObject[]): OverviewItemObject[]{
-    locationItems.forEach(
-      (location: OverviewItemObject) => {
-        const parents: OverviewItemObject[] = this.getParentLocations(location, locationItems);
-        const parentNames: string[] = parents.map((location: OverviewItemObject) => location.name).reverse();
-        const locationPath: string = parentNames.join(" - ");
+  processLocationOverviewItems(
+    locationItems: OverviewItemObject[]
+  ): OverviewItemObject[] {
+    locationItems.forEach((location: OverviewItemObject) => {
+      const parents: OverviewItemObject[] = this.getParentLocations(
+        location,
+        locationItems
+      );
+      const parentNames: string[] = parents
+        .map((location: OverviewItemObject) => location.name)
+        .reverse();
+      const locationPath: string = parentNames.join(' - ');
 
-        location.name_full = locationPath;      
-      }
-    );
+      location.name_full = locationPath;
+    });
 
     this.sortLocationsByNameFull(locationItems);
     return locationItems;
   }
 
-  getParentLocations(location: OverviewItemObject, listItems: OverviewItemObject[]): OverviewItemObject[]{
+  getParentLocations(
+    location: OverviewItemObject,
+    listItems: OverviewItemObject[]
+  ): OverviewItemObject[] {
     const parents: OverviewItemObject[] = [location];
 
-    var currentLocation: OverviewItemObject = location; 
+    var currentLocation: OverviewItemObject = location;
 
-    while(currentLocation.parent_location_details.pk != null){ // aka hasParentLocation
-      const parentLocationPk: number = currentLocation.parent_location_details.pk;
-      const parentLocation: OverviewItemObject = this.getListItemByPk(parentLocationPk, listItems);
+    while (currentLocation.parent_location_details.pk != null) {
+      // aka hasParentLocation
+      const parentLocationPk: number =
+        currentLocation.parent_location_details.pk;
+      const parentLocation: OverviewItemObject = this.getListItemByPk(
+        parentLocationPk,
+        listItems
+      );
       parents.push(parentLocation);
 
       currentLocation = parentLocation;
@@ -199,38 +242,58 @@ export class ArticleOverviewComponent extends PermissionUtilityFunctionMixin imp
     return parents;
   }
 
-  getListItemByPk(pk: number, listItems: OverviewItemObject[]): OverviewItemObject{
+  getListItemByPk(
+    pk: number,
+    listItems: OverviewItemObject[]
+  ): OverviewItemObject {
     if (pk == null) return null;
 
-    const targetItem: OverviewItemObject = listItems.find((item: OverviewItemObject) => item.pk === pk);
+    const targetItem: OverviewItemObject = listItems.find(
+      (item: OverviewItemObject) => item.pk === pk
+    );
     return targetItem;
   }
 
-  sortLocationsByNameFull(locations: OverviewItemObject[]){
-    locations.sort((loc1: OverviewItemObject, loc2: OverviewItemObject) => (loc1.name_full > loc2.name_full) ? 1 : -1);
+  sortLocationsByNameFull(locations: OverviewItemObject[]) {
+    locations.sort((loc1: OverviewItemObject, loc2: OverviewItemObject) =>
+      loc1.name_full > loc2.name_full ? 1 : -1
+    );
   }
 
-  buildDiaryEntryNameForList(diaryEntry: OverviewItemObject): string{
+  buildDiaryEntryNameForList(diaryEntry: OverviewItemObject): string {
     const startWithSessionNumber: string = `#${diaryEntry.session_details.session_number}`;
-    let title: string = (diaryEntry.name != null) ? `- ${diaryEntry.name}` : "";
+    let title: string = diaryEntry.name != null ? `- ${diaryEntry.name}` : '';
 
-    if (Constants.isSmallScreen) return `${startWithSessionNumber} ${title}`
+    if (Constants.isSmallScreen) return `${startWithSessionNumber} ${title}`;
 
-    let daysCoveredByEntry: string = "";
-    if(diaryEntry.session_details.start_day != null && diaryEntry.session_details.end_day != null){
+    let daysCoveredByEntry: string = '';
+    if (
+      diaryEntry.session_details.start_day != null &&
+      diaryEntry.session_details.end_day != null
+    ) {
       const padLength: number = 3;
-      const startDay: string = this.padNumber(diaryEntry.session_details.start_day, padLength, "");
-      const endDay: string = this.padNumber(diaryEntry.session_details.end_day, padLength, "");
+      const startDay: string = this.padNumber(
+        diaryEntry.session_details.start_day,
+        padLength,
+        ''
+      );
+      const endDay: string = this.padNumber(
+        diaryEntry.session_details.end_day,
+        padLength,
+        ''
+      );
       daysCoveredByEntry = `- Days ${startDay}-${endDay}`;
     }
-    
+
     return `${startWithSessionNumber} ${daysCoveredByEntry} ${title}`;
   }
 
-  padNumber(num: number, padCount: number, paddingCharacter="0"): string{
+  padNumber(num: number, padCount: number, paddingCharacter = '0'): string {
     const overlengthString: string = paddingCharacter.repeat(padCount) + num;
-    return overlengthString.slice(padCount*-1);
+    return overlengthString.slice(padCount * -1);
   }
 
-
+  private focusFilterField() {
+    this.filterField.first.nativeElement.focus();
+  }
 }
